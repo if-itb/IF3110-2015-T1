024bb6fcca2a4c86e97ef7966a6b96e837395bfd
Wilhelmus <wilhelm_andrean_22@yahoo.com>
2015-10-03 01:01:47 +0700

    initial index and css

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel ="stylesheet" type="text/css" href="Index.css">
+    <title>Simple StackExchange</title>
+
+</head>
+
+<body>
+    <div id = "container">
+        <div id  = "header">
+            <span id  = "logo"> Simple StackExchange </span>
+            <span id = "search">
+                <input class = "searchbar" type = "text"/>
+                <input class = "button" type = "submit" value="Search"/>
+                <p>Cannot find what you are looking for? <a class="y_link" href = "">Ask here</a></p>
+            </span>
+        </div>
+        <div id = "content">
+
+        </div>
+        <div id = "footer">
+
+        </div>
+    </div>
+<body>
