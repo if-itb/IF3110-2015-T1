2b824a02406dae55080077dc6cba028d8d24cde8
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-10-02 15:06:23 +0700

    membuat script instalasi untuk memudahkan testing. Dibuat berdasarkan file yang sudah ada terlebih dahulu.

---
+++ UbuntuApacheInstall.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#sudo
+if (($EUID != 0)); then
+  if [[ -t 1 ]]; then
+    sudo "$0" "$@"
+  else
+    exec 1>output_file
+    gksu "$0 $@"
+  fi
+  exit
+fi
+
+#some constants
+readonly webserverHTMLdir=/var/www/html
+readonly installdir=$webserverHTMLdir/SimpleStackExchange
+
+#check whether directory exists
+if [ ! -d "$webserverHTMLdir" ]; then
+  exit
+fi
+
+#creating directories
+[ -d $installdir ] || mkdir $installdir
+
+#installed files
+filesToCopy=( SubmitQuestion.php QuestionForm.html )
+
+#start copying files
+for i in "${filesToCopy[@]}"
+do
+  echo "Copying $i to $installdir/$i ..."
+  cp $i $installdir/$i
+  echo "Done"
+done
