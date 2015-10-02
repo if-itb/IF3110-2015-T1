ef52f2ed71035ed072a7dc9a502c1f65832585c1
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-10-02 15:05:38 +0700

    QuestionForm (form untuk mengajukan pertanyaan) first look

---
+++ SubmitQuestion.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>Submit Question - Simple StackExchange</title>
+ </head>
+ <body>
+ <?php echo '<h1>Not Implemented</h1>';
+	echo '<h2>got data:</h2>';
+	echo '<p> Name:'.$_POST['Name'];
+	echo '<p> Email:'.$_POST['Email'];
+	echo '<p> Question Topic:'.$_POST['QuestionTopic'];
+	echo '<p> Content:'.$_POST['Content'];
+
+ ?> 
+ </body>
+</html>
+
