dd2e101d983fe467537bcb86ab7bbf1fb107456a
Wilhelmus <wilhelm_andrean_22@yahoo.com>
2015-10-03 01:49:06 +0700

    add another page without logic

---
+++ AddQuestion.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel ="stylesheet" type="text/css" href="Index.css">
+    <title>Simple StackExchange</title>
+
+</head>
+
+<body>
+<div id = "container">
+    <div id  = "header">
+        <span id  = "logo"> Simple StackExchange </span>
+    </div>
+    <div id = "content">
+        <h2>What's Your Question?</h2>
+        <!--<form action="">-->
+            <input type = "text" name = "name" placeholder = "Name"/>
+            <input type = "text" name = "email" placeholder = "Email"/>
+            <input type = "text" name = "topic" placeholder ="Question Topic"/>
+        <!--</form>-->
+            <textarea name = "content" placeholder = "Content"></textarea>
+            <input class = "button" id="button_post" type = "submit" value="Post"/>
+    </div>
+    <div id = "footer">
+
+    </div>
+</div>
+<body>
--- index.php
+++ index.php
@@ -13,11 +13,11 @@
             <span id = "search">
                 <input class = "searchbar" type = "text"/>
                 <input class = "button" type = "submit" value="Search"/>
-                <p>Cannot find what you are looking for? <a class="y_link" href = "">Ask here</a></p>
+                <p>Cannot find what you are looking for? <a class="y_link" href = "AddQuestion.php">Ask here</a></p>
             </span>
         </div>
         <div id = "content">
-
+            <h3>Recently Asked Questions</h3>
         </div>
         <div id = "footer">
 
