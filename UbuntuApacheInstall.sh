2645bfc5c8621629b49149b77f3c510ab34a6aa1
Muhammad Nizami <nizami_muhammad@yahoo.co.id>
2015-10-03 18:35:01 +0700

    add confirmations to the install shell.

--- UbuntuApacheInstall.sh
+++ UbuntuApacheInstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#some constants
+readonly webserverHTMLdir=/var/www/html
+readonly installdir=$webserverHTMLdir/SimpleStackExchange
+
+
 #sudo
 if (($EUID != 0)); then
   if [[ -t 1 ]]; then
@@ -11,15 +16,28 @@ if (($EUID != 0)); then
   exit
 fi
 
-#some constants
-readonly webserverHTMLdir=/var/www/html
-readonly installdir=$webserverHTMLdir/SimpleStackExchange
+#prompt confirmation
+echo "Please make sure you have these and its dependencies installed:
++> apache2
++> php5-cgi
++> mysql
+Please also make sure that the apache2 home directory is in $webserverHTMLdir"
+while true
+do
+    read -r -p 'Do you want to continue? (y/n)' choice
+    case "$choice" in
+      n|N) exit;;
+      y|Y) break;;
+      *) echo 'Response not valid';;
+    esac
+done
 
 #check whether directory exists
 if [ ! -d "$webserverHTMLdir" ]; then
   exit
 fi
 
+#this part is for copying files to apache2 root Directory
 #creating directories
 [ -d $installdir ] || mkdir $installdir
 
@@ -33,3 +51,8 @@ do
   cp $i $installdir/$i
   echo "Done"
 done
+
+#this part is for setting up mysql database
+echo "creating database..."
+
+echo "done"
