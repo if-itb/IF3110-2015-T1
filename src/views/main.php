28aefd643e0eeabd2735fcbf3ac313e08939364f
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2015-10-06 00:56:44 +0700

    add track on /views folder

---
+++ src/views/main.php
@@ -0,0 +1,8 @@
+
+
+<form action="/index.php/questions/search" method="get">
+	<label for="q">Search:</label>
+	<input name="q" id="q" placeholder="Search">
+	<input type="submit" value="cari">
+</form>
+
