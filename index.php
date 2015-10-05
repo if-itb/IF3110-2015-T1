a12aab57ad8fbe8907d297bfb238aed9eecac0ac
Adin Baskoro <master.adin@gmail.com>
2015-10-05 17:03:51 +0700

    Added mainpage layout

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>My StackExchange</title>
+<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<h1 id="title">My StackExchange</h1>
+		<div class="search">
+			<form action="search.php" method="post">
+				<input type="text" id="searchbar">
+				<input type="submit" id="searchsubmit" value="Search">
+			</form>
+			<br>
+			Can't find what you are looking for? <a href="#">Ask here!</a>
+		</div>
+		<div class="content">
+			<h2>Recently Asked Questions</h2>
+			<hr>
+			<div class="stack">
+				<div class="votes"><div>0</div>Votes</div>
+				<div class="answers"><div>0</div>Answers</div>
+				<div class="question">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+				<div class="detail">asked by <a class="linkname">name</a> | <a class="linkedit">edit</a> | <a class="linkdelete">delete</a></div>
+				<hr>
+			</div>
+			<div class="stack">
+				<div class="votes"><div>0</div>Votes</div>
+				<div class="answers"><div>0</div>Answers</div>
+				<div class="question">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+				<div class="detail">asked by <a class="linkname">name</a> | <a class="linkedit">edit</a> | <a class="linkdelete">delete</a></div>
+				<hr>
+			</div>
+		</div>	
+	</div>
+</body>
+</html>
