2924fcb1ddb657710e0d91d19d4e9d08f82737b9
Ibrohim Kholilul Islam <ibrohimislam@gmail.com>
2015-10-06 02:22:37 +0700

    implement data passing to view

--- src/system/loader.php
+++ src/system/loader.php
@@ -6,9 +6,22 @@ Class Loader {
 
 		include_once "models/$src.php";
 
+		$srcArray = explode('/', $src);
+
+		$srcArrayLastIndex = count($srcArray) - 1;
+
+		$modelName = $srcArray[$srcArrayLastIndex];
+
+		$btrace = debug_backtrace();
+		$caller = $btrace[1]["object"];
+
+		$caller->$modelName = new $modelName();
 	}
 
 	public function view($src, $data =  array()){
+
+		foreach ($data as $key => $value)
+			$$key = $value;
 
 		include_once "views/$src.php";
 
