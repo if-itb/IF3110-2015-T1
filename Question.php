660212d78ca8eebb89df6c784e330dd227004eb2
deredet <lucky.cahyadi@yahoo.com>
2015-10-05 22:27:19 +0700

    add proto style & question form

---
+++ Question.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Simple Stack Excahange </title>
+		<link rel="stylesheet" type="text/css" href="Style.css">
+	</head>
+	<body>
+
+		<h1>Simple Stack Exchange</h1><br>
+		<h2>What's your question</h2>
+		<form action="Submit.php" method="post">
+			<div class="formpos">
+				<input type="text" name="name" placeholder="Nama" class="form-textfield"></input>
+			
+			<br>
+			<input type="text" name="email" placeholder="Email" class="form-textfield">
+			<br>
+			<input type="text" name="qtopic" placeholder="Question Topic" class="form-textfield">
+			<br>
+			<textarea name="comment" placeholder="Content" class="form-textarea">Content			</textarea>
+			</div>
+		</form>
+
+	</body>
+</html> 
