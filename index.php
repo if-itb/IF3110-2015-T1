3fa217b0230224318d9f3ac73f8fe60a4151e653
edwinwijaya94 <edwinwijaya94@yahoo.com>
2015-10-05 22:45:21 +0700

    Add index.php, index.css

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" href="css/index.css">
+	<title >
+		Simple StackExchange
+	</title>
+</head>
+<body>
+	<div id="TopDiv">
+		<h1 id="MainTitle">Simple StackExchange</h1> 
+		<br>
+		<form id="SearchBar">
+			<input id="SearchInput" type="text" name="SearchInput" placeholder="Search here...">&nbsp
+			<input id="SearchButton" type="submit" value="Search">
+		</form> 
+		<h5 id="find">Cannot find what you are looking for? &nbsp <a href="askQuestion.php" id="AskHere">Ask here</a></h5>
+		<br>
+	</div>
+	<div id="BottomDiv">
+		<h3 id="RAQ">Recently Asked Question</h3>
+		<hr>
+	</div>
+	<?php>
+		
+	<?>
+</body>
+</html>
