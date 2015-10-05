771848d1cda836fb234df5e080771ef1a0d772b2
Zeradok <cliffsantoso@yahoo.com>
2015-10-05 17:16:40 +0800

    Create index.php + css
    Initial appearance of the website

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link rel = "stylesheet" type = "text/css" href = "style.css">
+<title>Simple StackExchange</title>
+</head>
+<body>
+<div style = "width:60%; margin:auto;" align = "center">
+<h1>Simple StackExchange</h1>
+<div class = "searchdiv" style = "width: 100%;"><input type = "text"> <input type = "submit" value = "Search"></div>
+Cannot find what you are looking for? <a class = "yellow">Ask here</a><br><br>
+<h2>Recently Asked Questions</h2>
+<table>
+<tr>
+<td class = "votes">0<br>Votes</td>
+<td class = "answers">0<br>Answers</td>
+<td class = "question">The question topic goes here</td>
+<td class = "asked">asked by <a class = "blue">name</a>|<a class = "yellow">edit</a>|<a class = "red">delete</a></td>
+</tr>
+<tr>
+<td class = "votes">0<br>Votes</td>
+<td class = "answers">0<br>Answers</td>
+<td class = "question">The question topic goes here</td>
+<td class = "asked">asked by <a class = "blue">name</a>|<a class = "yellow">edit</a>|<a class = "red">delete</a></td>
+</tr>
+<tr>
+<td class = "votes">0<br>Votes</td>
+<td class = "answers">0<br>Answers</td>
+<td class = "question">The question topic goes here</td>
+<td class = "asked">asked by <a class = "blue">name</a>|<a class = "yellow">edit</a>|<a class = "red">delete</a></td>
+</tr>
+<tr class = "lasttr">
+<td class = "votes">0<br>Votes</td>
+<td class = "answers">0<br>Answers</td>
+<td class = "question">The question topic goes here</td>
+<td class = "asked">asked by <a class = "blue">name</a>|<a class = "yellow">edit</a>|<a class = "red">delete</a></td>
+</tr>
+</table>
+</div>
+</body>
+</html>
