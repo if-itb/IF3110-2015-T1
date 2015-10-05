45890c70e1b58d9889dfc1f70490873a2a7485ed
Ivan Andrianto <andrianto.ivan@yahoo.co.id>
2015-10-05 18:07:19 +0700

    tes

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="images/favicon.ico">
+		<title>Simple StackExchange</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="assets/style.css">
+	</head>
+	<body>
+		<div class="main">
+			<div class="site-title">
+				<center>
+					<h1>Simple StackExchange</h1>
+				</center>
+			</div>
