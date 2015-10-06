3699c3bd378cd6a3abfe7c1cfc324b8d875e279c
dininta <dinintaannisa@yahoo.com>
2015-10-06 13:54:07 +0700

    Add AskQuestion html

---
+++ AskQuestion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page Title</title>
+	</head>
+	<body>
+		<h2 align="center"> Simple StackExchange </h2>
+		<h2> What's your question? </h2>
+		<form method="POST">
+			<label for="nama">Name</label>
+			<input type="text" name="nama" id="nama"><br>
+			<label for="email">Email</label>
+			<input type="text" name="email" id="email"><br>
+			<label for="topic">Question Topic</label>
+			<input type="text" name="topic" id="topic"><br>
+			<label for="content">Content</label>
+			<textarea name="topic" id="topic"></textarea><br>
+			<input type="submit" value="Post">
+		</form>
+	</body>
+</html>
