9873756bdbd3fbe77beb8da2903843102b3e9f42
Astahta <fiqi9e@gmail.com>
2015-10-06 17:33:54 +0700

    html untuk halaman index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>StackExchange</title>
+	<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div class="container">
+
+	<h1>Simple StackExchange</h1>
+		<form action="">
+			<input id="search-box" type="text" name="searchbox">
+			<input id type="submit" value="Search">
+		</form>
+		Canot find what you are looking for? <a href="#">Ask here</a>
+</div>
+</body>
+</html>
