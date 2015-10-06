3dad7775ab3bafd5a7f7b1d38838f543c2eb15f5
Astahta <fiqi9e@gmail.com>
2015-10-06 13:45:44 +0700

    Add page html AskHere

---
+++ AskHere.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>AskHere</title>
+	
+</head>
+<body>
+	<div class="container">
+		<h1>Simple StackExchange</h1>
+		<h2>What's your question?</h2>
+		<hr>
+		<div class="questionform">
+			<form action="" method="post">
+				<input type="text" name="Name" placeholder="Name" >
+				<input type="text" name="Email" placeholder="Email">
+				<input type="text" name="QuestionTopic" placeholder="Question Topic">
+				<textarea placeholder= "Content" name="message"  ></textarea>
+				<input class="button" type="submit" name="Post" value="Post">
+			</form>
+		</div>
+	</div>	
+</body>
+</html>
