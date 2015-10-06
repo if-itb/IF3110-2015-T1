d3495019640d70ffdf1d82fcba7e5dab3bbddd56
Astahta <fiqi9e@gmail.com>
2015-10-06 16:23:27 +0700

    Menampilkan Pertanyaan & info pada halaman Answer

--- Answer.php
+++ Answer.php
@@ -5,21 +5,62 @@
 	<link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-	<?php include("DB_connection.php"); ?>
+	<?php 
+		
+
+		//dummy id question
+		$ID_Q= 1;
+		
+		
+		function readQuestion($id_Question){
+			$servername = "localhost";
+			$username = "root";
+			$password = "";
+			$dbname = "stackexchange";
+			// Create connection
+			$conn = mysqli_connect($servername, $username, $password, $dbname);
+			// Check connection
+			if (!$conn) {
+				die("Connection failed: " . mysqli_connect_error());
+			}
+			$sql = "SELECT Content, Vote, Email, Date FROM question WHERE ID_Question ='$id_Question'";
+			$result = mysqli_query($conn, $sql);
+			if (mysqli_num_rows($result) > 0) {
+				// output data of each row
+				while($row = mysqli_fetch_assoc($result)) {
+				echo '<div class= "resQuestion">';
+				echo "" . $row["Content"];
+				echo "</div>";
+				echo '<div class= "infoQuestion">';
+				echo " asked by " . $row["Email"]. " at " . $row["Date"]. "<a href='#'>| edit |</a> ". "<a href='#'>| delete |</a>";
+				echo "</div>";
+			}
+			} else {
+				echo "0 results";
+			}
+			
+		}
+	?>
 	
 	<div class="container">
 		<h1>Simple StackExchange</h1>
+		<br><br><br>
 		
-		<hr>
 		<div class="question">
+				<div class="SubTitle">
+				<h2>The question topic goes here </h2>
+				</div>
 				<div class="vote">
-					
+					1
 				</div>
 				<div class="content">
-				
+					<?php readQuestion(1);?>
 				</div>
 		</div>
 		<div class="answer">
+				<div class="SubTitle">
+				<h2>Answer</h2>
+				</div>
 				<div class="vote">
 				
 				</div>
