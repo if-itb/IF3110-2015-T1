cacc6ed91f3d7ce79b6fe8180a51570ecb7754d1
Ignatius Alriana Haryadi Moel <alrianatcm@gmail.com>
2015-10-07 01:47:54 +0700

    Create the question form template

---
+++ ask.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--@author : Ignatius Alriana H.M / 13513051-->
+<html>
+	<head>
+		<title>Simple StackExcahange | Ask</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+	<body>
+		<form action="post.php" method="post">
+		<a href="index.php"><h1>Simple StackExcahange</h1></a>
+		<table align="center">
+			<tr><td>
+				<h2>What's your question?</h2>
+			</td></tr>
+			<tr><td>
+				<div class="garis"></div>
+			</td></tr>
+			<tr><td>
+				<input type="text" name="Name" class="form-field" placeholder="Name"></input>
+			</tr></tr>
+			<tr><td>
+				<input type="text" name="Email" class="form-field" placeholder="Email"></input>
+			</tr></tr>
+			<tr><td>
+				<input type="text" name="Topic" class="form-field" placeholder="Question Topic"></input>
+			</tr></tr>
+			<tr><td>
+				<textarea name="comment" placeholder="Content" class="form-textarea"></textarea>
+			</tr></tr>
+			<tr><td align="right">
+				<input class="button" type="button" value="Post">
+			</tr>
+		</table>
+		</form>
+	</body>
+</html> 
