fc69e75fb325c5682c575d1e9d599e7ec853c465
Husni Munaya <husnimun@gmail.com>
2015-10-07 16:20:32 +0700

    Add styling for homepage

--- index.php
+++ index.php
@@ -1,58 +1,79 @@
 <?php include( 'template/header.php' ); ?> 
-  <header>
-    <h1>Simple StackExchange</h1>
-  </header>
-  
-  <h2>Recently Asked Questions</h2>
-  
-  <hr>
 
-  <div class="question-list">
+  <div class="container">
     
-    <div class="question">
-      
-      <div class="question-status">
-        <div class="question-vote">
-          <div class="status-counts">
-            <span>0</span>
-          </div>
-
-          <div class="status-title">
-            <span>vote</span>
-          </div>
-        </div>
-        
-        <div class="question-answer">
-          <div class="status-counts">
-            <span>0</span>
-          </div>
-
-          <div class="status-title">
-            <span>answer</span>
-          </div>
-        </div>
-      </div>
-      
-      <div class="question-summary">
-        
-        <h2 class="question-title">
-          <a href="question.php">Question title</a>
-        </h2>
-        
-        <div class="question-meta">
-          <p>
-            Asked by
-            Name |
-            <a href="edit.php" class="question-edit">Edit</a> |
-            <a href="" class="question-delete">Delete</a>
-          </p>
-        </div>
-      
-      </div>
+    <header class="main-title">
+      <h1>Stack<span>Exchange<span></h1>
+    </header>
     
-    </div><!-- .question -->
 
-    <hr>
+    <div class="row">
+      <div class="secondary-title col-10 col-push-1">
+        <h2>Recently Asked Questions</h2>
+      </div>  
+    </div>
+    
+
+    <div class="question-list">
+      
+      <div class="row">
+
+        <div class="col-10 col-push-1">
+
+          <div class="question">
+
+            <div class="row">
+              
+              <div class="question-status col-3">
+                <div class="question-vote">
+                  <div class="status-counts">
+                    <span>0</span>
+                  </div>
+
+                  <div class="status-title">
+                    <span>vote</span>
+                  </div>
+                </div>
+                
+                <div class="question-answers">
+                  <div class="status-counts">
+                    <span>0</span>
+                  </div>
+
+                  <div class="status-title">
+                    <span>answer</span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="question-summary col-9">
+                
+                <h2 class="question-title">
+                  <a href="question.php">Question title</a>
+                </h2>
+                
+                <div class="question-meta">
+                  <p>
+                    Asked by
+                    Name |
+                    <a href="edit.php" class="question-edit">Edit</a> |
+                    <a href="" class="question-delete">Delete</a>
+                  </p>
+                </div>
+              
+              </div>
+
+            </div> <!-- .row -->
+          
+          </div> <!-- .question -->
+          
+        </div> <!-- .col -->
+
+      </div> <!-- .row -->
+
+    
+    </div> <!-- .question-list -->
+
+  </div> <!-- .container -->
   
-  </div> <!-- .question-list -->
 <?php include( 'template/footer.php' ); ?> 
