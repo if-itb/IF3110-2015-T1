16be4b4a1740540a23a9dae52084c15568779a1e
Luqman A. Siswanto <l.arifin.siswanto@gmail.com>
2015-10-07 20:48:16 +0700

    Add homepage CSS

--- src/view/index.php
+++ src/view/index.php
@@ -6,20 +6,83 @@
 
   </head>
   <body>
-    <h1>OVERFLOW48</h1>
-    <p>Cannot find what you are looking for? Ask here.</p>
-
-    <h2>Recently Asked Questions</h2>
-    <hr/>
-    <?php
+    <div class="container">
+      <h1 class="text-center">OVERFLOW48</h1>
       
-      require "../conf.php";
-      $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_DATABASE);
-      if($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-      }
-      echo "Connected successfully";
-    ?>
+      <form action="/controller/search.php" action="GET">
+        <table>
+        <tr>
+          <td width="200%"> <input type="text" class="form" name="q"></td>
+          <td width="20%"> <button class="button" type="submit">Search</button> </td>
+        </tr>
+        </table>
+      </form>
 
+      <p class="text-center">Cannot find what you are looking for? Ask here.</p>
+
+      <br/> <h2>Recently Asked Questions</h2>
+
+      
+      <div class="question">
+        <hr class="line">
+        <div class="item">
+          <div class="vote">
+            <p class="text-center">5</p>
+            <p class="text-center">Votes</p>
+          </div>
+          <div class="answer">
+            <p class="text-center">0</p>
+            <p class="text-center">Answers</p>
+          </div>
+          <div class="text">
+            <p>Overflow48 baru saja rilis. Apakah perusahaan ini related dengan AKB48?</p>
+          </div>
+        </div>
+        <div class="text-right">
+          asked by Luqman | edit | delete
+        </div>
+      </div>
+
+      <div class="question">
+        <hr class="line">
+        <div class="item">
+          <div class="vote">
+            <p class="text-center">-6</p>
+            <p class="text-center">Votes</p>
+          </div>
+          <div class="answer">
+            <p class="text-center">27</p>
+            <p class="text-center">Answers</p>
+          </div>
+          <div class="text">
+            <p>Need help. assert(anjing != asu) gives me assertion failed.</p>
+          </div>
+        </div>
+        <div class="text-right">
+          asked by Kadal Blinksatan | edit | delete
+        </div>
+      </div>
+
+      <div class="question">
+        <hr class="line">
+        <div class="item">
+          <div class="vote">
+            <p class="text-center">2</p>
+            <p class="text-center">Votes</p>
+          </div>
+          <div class="answer">
+            <p class="text-center">1</p>
+            <p class="text-center">Answers</p>
+          </div>
+          <div class="text">
+            <p>Resep kentang goreng 2015</p>
+          </div>
+        </div>
+        <div class="text-right">
+          asked by Inas Nuha | edit | delete
+        </div>
+      </div>
+
+    </div>
   </body>
 </html>
