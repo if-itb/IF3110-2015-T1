503fbd1169bdaee536d4dbc37651925c560e2f3e
Ignatius Alriana Haryadi Moel <alrianatcm@gmail.com>
2015-10-07 12:38:35 +0700

    fixing margin

--- index.php
+++ index.php
@@ -2,42 +2,18 @@
 <!--@author : Ignatius Alriana H.M / 13513051-->
 <html>
 	<head>
-		<title>Simple StackExcahange</title>
+		<title>Simple StackExchange</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 
 	<body>
-		<a href="index.php"><h1>Simple StackExcahange</h1></a>
-		<table align="center">
-			<tr>
-				<td>
-					<input type="text" class="dummy"></input>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<p align="center"><input type="text" name="search" class="search" ></input>
-					<input class="button" type="button" value="Search"></p>
-				<td>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<h5>Cannot find what you are  looking for? <a href="ask.php"><span id="link">Ask here</span></a></h5>
-				</td>
-			</tr>
-			<tr>
-				<td>
-				<h4>Recently Asked Questions</h4>
-				</td>
-			</tr>
-			<tr>
-				<td>
-				<div class="garis"></div>
-				</td>
-			</tr>
-
-		</table>
-
+		<a href="index.php"><h1>Simple StackExchange</h1></a>
+		<p align="center"><input type="text" name="search" class="search" ></input>
+		<input class="button" type="button" value="Search"></p>
+		
+		<h5>Cannot find what you are  looking for? <a href="ask.php"><span id="link">Ask here</span></a></h5>
+		<h4>Recently Asked Questions</h4>
+		
+		<div class="garis"></div>
 	</body>
 </html> 
