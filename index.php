c4ab18005c01c9664aaef0c837f2b75c9d417306
Asanilta Fahda <asanilta@gmail.com>
2015-10-07 23:29:08 +0700

    Mulai bikin :D
    - Bikin tampilan (kasar) halaman index
    - Bikin tampilan (kasar) halaman new post
    - Validasi new post

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Simple StackExchange</title>
+
+    <link href="css/style.css" rel="stylesheet">
+
+</head>
+<body>
+	<div class="container">
+        <h1>Simple StackExchange</h1>
+        <input type="search" name="forum_search" id="forum_search"><button type="submit">Search</button><br>
+        <div class="text-center">Cannot find what you're looking for? <a href="#">Ask here</a></div>
+        <h3>Recently Asked Questions</h3>
+        
+        <hr>
+        <div class="question_block">
+            <div class="votes">0<br>Votes</div>
+            <div class="answers">0<br>Answers</div>
+            <div class="topic">The question topic goes here The question topic goes here The question topic goes here</div>
+            <div class="details">asked by name | edit | delete</div>    
+        </div>
+        <hr>
+        <div class="question_block">
+            <div class="votes">0<br>Votes</div>
+            <div class="answers">0<br>Answers</div>
+            <div class="topic">The question topic goes here The question topic goes here The question topic goes here</div>
+            <div class="details">asked by name | edit | delete</div>    
+        </div>
+        <hr>
+        <div class="question_block">
+            <div class="votes">0<br>Votes</div>
+            <div class="answers">0<br>Answers</div>
+            <div class="topic">The question topic goes here The question topic goes here The question topic goes here</div>
+            <div class="details">asked by name | edit | delete</div>    
+        </div>
+        
+
+    </div>
+</body>
+</html>
---
+++ newpost.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Simple StackExchange</title>
+
+    <link href="css/style.css" rel="stylesheet">
+
+    <script>
+	function validateForm() {
+    	var q_name = document.forms["q_form"]["q_name"].value;
+    	var q_email = document.forms["q_form"]["q_email"].value;
+    	var q_topic = document.forms["q_form"]["q_topic"].value;
+    	var q_content = document.getElementById("q_content").value;
+    	if (q_name == null || q_name == "" || q_email == null || q_email == "" || q_topic == null || q_topic == "" || q_content == null || q_content == "") {
+        	alert("All fields must be filled out");
+        	return false;
+    	} else if (!validateEmail(q_email)) {
+    		alert("Email address not valid");
+    		return false;
+    	}
+	}
+	function validateEmail(email) {
+		var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+		return re.test(email);
+	}
+	</script>
+</head>
+<body id="question">
+	<div class="container">
+    <h1>Simple StackExchange</h1>
+    <h2>What's your question?</h2>
+    <hr>
+    <form action="submit_question.php" method="post" id="q_form" onsubmit="return validateForm()">
+        <input type="text" class="form-control" placeholder="Name" name="q_name"><br>
+        <input type="text" class="form-control" placeholder="Email" name="q_email"><br>
+        <input type="text" class="form-control" placeholder="Question Topic" name="q_topic"><br>
+        <textarea id="q_content" name="q_content" placeholder="Content"></textarea><br>
+        <button type="submit">Post</button>
+    </form>
+    </div>
+</body>
+
+</html>
