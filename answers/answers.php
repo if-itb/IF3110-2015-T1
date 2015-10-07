809d9a08ebaf950b3bd2c009142e1788f38842be
miftahulmahfuzh <miftahulmahfuzh@gmail.com>
2015-10-07 21:14:31 +0700

    integrated with database answers

--- answers/answers.php
+++ answers/answers.php
@@ -6,44 +6,50 @@
 </head>
 <body>
 	<div id="big">Simple StackExchange</div>
-	<div class="medium" id="m1">The question topic goes here</div>
-	<div class="medtab"><table>
-		<tr>
-			<td id="num">
-				<table>
-					<tr><td>▲</td></tr>
-					<tr><td>2</td></tr> 
-					<tr><td>▼</td></tr>
-				</table>
-			</td>
-			<td id="anscontent">Anda diminta untuk membuat tampilan sedemikian hingga mirip dengan tampilan berikut. 
-			Website yang diminta tidak harus responsive. Desain tampilan tidak perlu dibuat indah. 
-			Icon dan jenis font tidak harus sama dengan contoh. Warna font, garis pemisah, 
-			dan perbedaan ukuran font harus terlihat sesuai contoh. 
-			Perhatikan juga tata letak elemen-elemennya.</td>
-		</tr>
-		<tr>
-			<td id="asker">asked by mahfuzh74@gmail.com at 23/11/2015 23:23 | edit | delete</td>
-		</tr>
-	</table></div>
-	<div class="medium" id="m1">1 Answer</div>
-	<div class="medtab"><table>
-		<tr>
-			<td id="num">
-				<table>
-					<tr><td>▲</td></tr>
-					<tr><td>6</td></tr> 
-					<tr><td>▼</td></tr>
-				</table>
-			</td>
-			<td id="anscontent">Perusahaan mesin pencari ini sampai menyempatkan diri datang ke kantor Go-Jek di bilangan
-			 Kemang untuk mendokumentasi sepak terjang pendiri Go-Jek, Nadiem Makarim, dan kedua rekannya tersebut. 
-			 Diceritakan oleh Michaelangelo Moran, salah satu pendiri Go-Jek, Google akan membuat video berisi kisah 
-			 seputar Go-Jek dan bagaimana Google Maps membantu layanan ojek digital tersebut.</td>
-		</tr>
-		<tr>
-			<td id="asker">answered by sumanto94@rocketmail.com at 24/12/2015 00:20</td>
-		</tr>
+	<?php $conn = new mysqli("localhost","root","","stackoverflow");
+	if($conn->connect_error)
+		die("Connection failed : " . $conn->connect_error);
+	$sql = "SELECT * FROM questions WHERE no=11";
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_assoc($result);
+	echo "<div class=\"medium\" id=\"m1\">". $row["question"] ."</div>";
+	echo "<div class=\"medtab\"><table>";
+		echo"<tr>";
+			echo"<td id=\"num\">";
+				echo"<table>";
+					echo"<tr><td>▲</td></tr>";
+					echo"<tr><td>". $row["vote"] ."</td></tr>";
+					echo"<tr><td>▼</td></tr>";
+				echo"</table>";
+			echo"</td>";
+			echo"<td id=\"anscontent\">". $row["content"] ."</td>";
+		echo"</tr>";
+		echo"<tr>";
+			echo"<td id=\"asker\">asked by ". $row["email"] ." at ". $row["time"] ." <a href=\"../questions/questions.php\">edit</a> | delete</td>";
+		echo"</tr>";
+	echo"</table></div>"; 
+	$sum = "SELECT COUNT(*) AS SHIT FROM answers WHERE question_no=11";
+	$ans = mysqli_query($conn, $sum);
+	while ($row=$ans->fetch_assoc()) {
+	echo"<div class=\"medium\" id=\"m1\">".$row['SHIT'] ." Answer</div>"; }
+	$sql = "SELECT * FROM answers WHERE question_no=11";
+	$result = mysqli_query($conn, $sql);
+	while($row = mysqli_fetch_assoc($result)) {
+	echo"<div class=\"medtab\"><table>";
+		echo"<tr>";
+			echo"<td id=\"num\">";
+				echo"<table>";
+					echo"<tr><td>▲</td></tr>";
+					echo"<tr><td>". $row["vote"] ."</td></tr> ";
+					echo"<tr><td>▼</td></tr>";
+				echo"</table>";
+			echo"</td>";
+			echo"<td id=\"anscontent\">". $row["content"] ."</td>";
+		echo"</tr>";
+		echo"<tr>";
+			echo"<td id=\"asker\">answered by ". $row["email"] ." at ". $row["time"] ."</td>";
+		} $conn->close();
+		?></tr>
 	</table></div>
 	<div class="medium" id="m2">Your Answer</div>
 	<form name="makequestion" method="post" action="sendanswers.php">
--- questions/sendquestions.php
+++ questions/sendquestions.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "questions";
+$dbname = "stackoverflow";
 //create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 //check connection
