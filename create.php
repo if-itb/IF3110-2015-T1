71dc5dee7b6ce577bf70bab20925b7b4edf1aefb
gbusiri <13513041@std.stei.itb.ac.id>
2015-10-07 11:07:12 +0700

    make homepage

---
+++ create.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tugas WBD</title>
+        <link type="text/css" rel="stylesheet" href="style.css" >
+	</head>
+
+	<body>
+
+		<h1 id="judul">Simple StackExchange</h1>
+		<div id="garis">
+		<p>What's Your Question?</p>
+		</div>
+
+		<div id="form">
+		<form>
+			<br>
+			<input type="text" placeholder="Name" name="name" class="box">
+			<br>
+			<br>
+			<input type="text" placeholder="Email" name="email" class="box">
+			<br>
+			<br>
+			<input type="text" placeholder="Question" name="question" class="box">
+			<br>
+			<br>
+			<textarea placeholder="Content" name="content" class="box" rows="5" cols="22"></textarea>
+			<br>
+			<input type="button" value="Post" name="post" id="posisipost">
+		</form>
+		</div>
+
+	</body>	
+
+</html>
