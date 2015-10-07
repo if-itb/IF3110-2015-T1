89bc9a61289197f1f166101c416ec511102e7d72
Ignatius Alriana Haryadi Moel <alrianatcm@gmail.com>
2015-10-07 12:39:17 +0700

    add validate form in javascript

--- ask.php
+++ ask.php
@@ -4,34 +4,26 @@
 	<head>
 		<title>Simple StackExcahange | Ask</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
+		<script type="text/javascript" src="js/validate.js"></script>
 	</head>
 
 	<body>
-		<form action="post.php" method="post">
-		<a href="index.php"><h1>Simple StackExcahange</h1></a>
-		<table align="center">
-			<tr><td>
-				<h2>What's your question?</h2>
-			</td></tr>
-			<tr><td>
-				<div class="garis"></div>
-			</td></tr>
-			<tr><td>
-				<input type="text" name="Name" class="form-field" placeholder="Name"></input>
-			</tr></tr>
-			<tr><td>
-				<input type="text" name="Email" class="form-field" placeholder="Email"></input>
-			</tr></tr>
-			<tr><td>
-				<input type="text" name="Topic" class="form-field" placeholder="Question Topic"></input>
-			</tr></tr>
-			<tr><td>
-				<textarea name="comment" placeholder="Content" class="form-textarea"></textarea>
-			</tr></tr>
-			<tr><td align="right">
-				<input class="button" type="button" value="Post">
-			</tr>
-		</table>
+		<a href="index.php"><h1>Simple StackExchange</h1></a>
+		<h2>What's your question?</h2>
+		<div class="garis"></div>
+		<form action="post.php"  method="post" name="ask-form">
+			<input type="text" name="Name" class="form-field" placeholder="Name"></input>
+			<br>
+			<input type="text" name="Email" class="form-field" id="Email" placeholder="Email"></input>
+			<br>
+			<input type="text" name="Topic" class="form-field" placeholder="Question Topic"></input>
+			<br>
+			<textarea name="comment" placeholder="Content" class="form-textarea"></textarea>
+			<br>
 		</form>
+		<div align="right">
+			<input type="submit" value="Post" align="right" onclick="return validateForm()"></input>
+		</div>
+	
 	</body>
 </html> 
