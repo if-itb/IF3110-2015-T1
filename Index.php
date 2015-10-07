fdc2ef590e6b196fa3a741611ebe3b2917d308d0
deredet <lucky.cahyadi@yahoo.com>
2015-10-07 11:28:53 +0700

    add Index

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Simple Stack Exchange </title>
+		<link rel="stylesheet" type="text/css" href="Style.css">
+	</head>
+	<body>
+		<h1>Simple Stack Exchange</h1><br>
+		<form action="Submit.php" method="post">
+			<div class="search">
+				<input type="text" name="name"  class="form-textfield"></input>
+				<input type="submit" value="Search">
+			</div>
+		</form>
+		<br>
+		<p id=style1>Cannot find what you are looking for? <a href="Question.php" id=linkstyle1>Ask here</a> </p> 
+		<h2>Recently Asked Questions</h2>
+		
+	</body>
+</html> 
