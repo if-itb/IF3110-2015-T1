a7bf730386baad4938462758df08639f8a8f9e44
Ignatius Alriana Haryadi Moel <alrianatcm@gmail.com>
2015-10-07 12:40:09 +0700

    create validate form function

---
+++ js/validate.js
@@ -0,0 +1,43 @@
+/* @Author	: Ignatius Alriana Haryadi Moel
+ * File		: validate.js
+ */
+
+ function validateForm(){
+
+ 	var fName = document.forms["ask-form"]["Name"].value;
+ 	var fEmail = document.forms["ask-form"]["Email"].value;
+ 	var fTopic = document.forms["ask-form"]["Topic"].value;
+ 	var fcomment = document.forms["ask-form"]["comment"].value;
+
+ 	// String allert
+ 	var s="";
+
+    if (fName == null || fName == "") {
+        s+="Name cannot be empty\n";
+    }
+    if (fEmail == null || fEmail == "") {
+        s+="Email cannot be empty\n";
+    } else {
+    	if(!validateEmail(fEmail)){
+    		s+="Email not valid\n";
+    	}
+    }
+    if (fTopic == null || fTopic == "") {
+        s+="Topic cannot be empty\n";
+    }
+    if (fcomment == null || fcomment == "") {
+        s+="Comment cannot be empty\n";
+    }
+
+    if(s!="") {
+    	alert(s);
+    	return false;
+    }
+
+ }
+
+ function validateEmail(fEmail){
+    var filter =/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+    return filter.test(fEmail)
+ }
