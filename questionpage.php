b0beba7c5a9b0bd8a1de71eeddaf7487d0d2944b
zulvaf <zz_vha91@yahoo.com>
2015-10-08 04:42:05 +0700

    First Commit
    Header Name and simple css

---
+++ questionpage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+	<title>Stack Exchange</title>
+	<link rel="StyleSheet" href="css/style.css" type="text/css">
+
+</head>
+
+<body>
+
+<div id="header">
+	<h1> Simple Stack Exchange </h1>
+</div>
+
+<div class = "container">
+
+</div>
+
+</body>
+</html>
