9a9ea4833ef1f2b511a8b26663f52cff4049430c
fauzannaufan <fauzannaufan@gmail.com>
2015-10-07 17:44:00 +0700

    Membuat form http post untuk mengirim pertanyaan
    Hasil http post dimuat di showask.php

---
+++ showask.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+Nama : <?php echo $_POST["name"] ?><br>
+Email : <?php echo $_POST["email"] ?><br>
+Question Topic : <?php echo $_POST["topic"] ?><br>
+Content : <?php echo $_POST["content"] ?><br>
+</body>
+</html>
