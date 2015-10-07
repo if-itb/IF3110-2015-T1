0a4466d7b0783e04698ed8431c82829e879e5d89
deredet <lucky.cahyadi@yahoo.com>
2015-10-07 11:27:08 +0700

    update

--- Question.php
+++ Question.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Simple Stack Excahange </title>
+		<title>Simple Stack Exchange </title>
 		<link rel="stylesheet" type="text/css" href="Style.css">
+		<script type="text/javascript" src = "Validate.js"></script>
 	</head>
 	<body>
 
 		<h1>Simple Stack Exchange</h1><br>
 		<h2>What's your question</h2>
-		<form action="Submit.php" method="post">
+		<form name="qForm" action="Submit.php" method="post" onsubmit="return validateForm()" >
 			<div class="formpos">
 				<input type="text" name="name" placeholder="Nama" class="form-textfield"></input>
-			
-			<br>
-			<input type="text" name="email" placeholder="Email" class="form-textfield">
-			<br>
-			<input type="text" name="qtopic" placeholder="Question Topic" class="form-textfield">
-			<br>
-			<textarea name="comment" placeholder="Content" class="form-textarea">Content			</textarea>
+				<br>
+				<input type="text" name="email" placeholder="Email" class="form-textfield">
+				<br>
+				<input type="text" name="qtopic" placeholder="Question Topic" class="form-textfield">
+				<br>
+				<textarea name="content" placeholder="Content" class="form-textarea"></textarea>
+				<br>
+				<div class="searchstyle">
+					<input type="submit" value="Submit">
+				</div>
 			</div>
 		</form>
 
