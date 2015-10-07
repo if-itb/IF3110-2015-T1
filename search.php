55ef4ce02773186bd76c638afb89e08d822d74da
mvisat <mvisat@gmail.com>
2015-10-07 18:42:21 +0700

    Fix search feature

--- search.php
+++ search.php
@@ -1,29 +1,23 @@
 <?php
-    require_once "db.php";
-
-    if (!empty($_GET["q"])) {
+    require_once "function.php";
+    if ($_SERVER["REQUEST_METHOD"] == "GET" && !empty($_GET["q"])) {
+        require_once "db.php";
         $search = $mysqli->real_escape_string($_GET["q"]);
         $query = "SELECT * FROM question WHERE content LIKE '%$search%' OR topic LIKE '%$search%'";
         $result = $mysqli->query($query);
         require_once 'header.php';
-        echo "Search for: " . $search;
-        while ($row = $result->fetch_assoc()) {
-            echo "<hr>";
-            $id_question = $row["id"];
-            $answer = $mysqli->query("SELECT id FROM answer WHERE id_question=$id_question");
-            echo $row["vote"] . " votes " . $answer->num_rows . " answers" . PHP_EOL;
-            $answer->close();
-            echo "<a href=question.php?id=". $row["id"] . ">". $row["topic"] ."</a>";
-            echo "<p>". $row["content"] . "</p>";
-            echo "asked by " . $row["name"] . " | <a href=ask.php?id='.$row["id"].'>edit</a> | delete";
-        }
-        echo "<hr>";
+        echo '<div class="container">';
+            echo '<h3>Search for: '. $search .'</h3>';
+            echo '<hr class="heading">';
+            while ($row = $result->fetch_assoc()) {
+                $query = "SELECT id FROM answer WHERE id_question='". $row["id"] ."'";
+                displayQuestionList($row["id"], $row["topic"], $row["name"], $row["email"], $row["content"], $row["time"], $row["vote"], $mysqli->query($query)->num_rows);
+            }
+        echo '</div>';
         $mysqli->close();
+        require_once "footer.php";
     }
     else {
-        $mysqli->close();
         backToHome();
     }
-
-    require_once "footer.php";
 ?>
