0af87b3d4f07c8c7d39b471b1cfbf6d81713a7ec
AlbertTriAdrian <alberttri23@gmail.com>
2015-10-07 20:31:20 +0700

    Fixing css and Test Database Connection

---
+++ create_question.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css" type="text/css">
+
+    <title>Simple StackExchange</title>
+</head>
+<body>
+<div id = "container">
+    <div id="header">
+      <p id="logo">Simple StackExchange</p>
+    </div>
+
+    <div id= "content">
+      <p id="content-title">What's your question?</p>
+      <hr style="width:90%">
+    
+      <!--javascript Form Validation -->
+      <form name="question_form" id="question" action="controller.php" onsubmit="return validateQuestionForm()" method="post"  > 
+         <input type="text" name="name" placeholder="Name">
+         <input type="text" name="email" placeholder="Email" >
+         <input type="text" name="topic" placeholder="Question Topic">
+         <textarea name="content" placeholder="Content" rows="5" cols="40"></textarea>
+         <input id="submit" type="submit" value="Post"> 
+      </form>
+    </div>
+
+    <div id="footer">
+    </div>
+    
+</div>
+</body>
+
+    <script type="text/javascript" src="script.js"></script>
+</html>
--- index.php
+++ index.php
@@ -1,44 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <link rel="stylesheet" href="/css/style.css" type="text/css" >
+    <link rel="stylesheet" href="style.css" type="text/css" >
 
     <title>Simple StackExchange</title>
 </head>
 <body>
-    <div id="header">
-        <p id="logo">Simple StackExchange</p>
-        <form id="search" method="get" action="http://www.google.com">
-                <input type="text" id="searchBar" name="searchBar" size="21" maxlength="120">
-                <button type="submit" id="searchButton" name="searchButton">Search</button>
-        </form>
-        <p>Cannot find what you are looking for? <a id="askHere" href="q_create.php">Ask here</a></p>        
+    <div id= "container">
+
+        <div id="header">
+            <p id="logo">Simple StackExchange</p>
+            <span id="search" >
+                    <input type="text" class ="searchBar">
+                    <input type="submit" class ="submitButton" value="Search">
+            </span>
+            <p>Cannot find what you are looking for?<a class="q_createLink" href="create_question.php">Ask here</a></p>        
+        </div>
+
+        <div id="content">
+            <p>Recently Asked Questions</p>
+            <hr>
+        </div>
+
+        <div id="footer">
+
+        </div>
+
     </div>
-
-
-
-    <div id="nav">
-        London<br>
-        Paris<br>
-        Tokyo<br>
-    </div>
-
-    <div id="section">
-        <h1>London</h1>
-        <p>
-        London is the capital city of England. It is the most populous city in the United Kingdom,
-        with a metropolitan area of over 13 million inhabitants.
-        </p>
-        <p>
-        Standing on the River Thames, London has been a major settlement for two millennia,
-        its history going back to its founding by the Romans, who named it Londinium.
-        </p>
-    </div>
-
-    <div id="footer">
-        Copyright © W3Schools.com
-    </div>
-
 </body>
+
     <script type="text/javascript" src="/js/script.js"></script>
 </html>
