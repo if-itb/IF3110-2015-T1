26616ed6e42952ac31d19cf644e3b4f00e2821f6
Husni Munaya <husnimun@gmail.com>
2015-10-08 13:30:03 +0700

    Update html structure, add style for question page

---
+++ question.php
@@ -0,0 +1,151 @@
+<?php include( 'template/header.php' ); ?> 
+
+  <div class="outer-container">
+    
+    <header class="main-title">
+      <h1>Stack<span>Exchange<span></h1>
+    </header>
+    
+    <form action="" method="GET" id="searchForm">
+      <input type="text" placeholder="Search...">
+      <input type="submit" value="Search">
+    </form>
+
+    <p class="ask-here">Can't find what you are looking for? <a href="">Ask here</a></p>
+
+    <div class="row">
+      <div class="question-header col-10 col-push-1">
+        <h1>Question Title</h1>
+      </div>  
+    </div>
+    
+    <div class="inner-container">
+        
+      <div class="question-item">
+        
+        <div class="row">
+          
+          <div class="question-status col-2">
+            
+            <div class="vote">
+              <div class="vote-up">
+                <a class="vote-link" href="">▲</a>  
+              </div>
+
+              <div class="vote-counts">
+                <span>0</span>  
+              </div>
+
+              <div class="vote-down">
+                <a class="vote-link" href="">▼</a>
+              </div>
+            </div>
+
+          </div> <!-- .question-status -->
+
+          <div class="question-content col-10">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat rem quibusdam nihil sequi odit doloremque. Nemo eaque alias repudiandae, consequatur itaque ea pariatur aliquid quasi quo quos, ducimus deserunt eum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, dolorum recusandae dolore delectus expedita, eum eveniet facilis libero rem obcaecati sit ipsam illo doloribus ratione, deleniti tenetur hic aperiam enim.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, labore quia velit, sint pariatur beatae reiciendis, aperiam sit totam, iste ea sequi. Excepturi, inventore quidem? Rerum mollitia, necessitatibus itaque laudantium?</p>
+          </div>
+
+          <div class="question-meta">
+            <span>
+              Asked by
+              Name |
+              <a href="edit.php" class="question-edit">Edit</a> |
+              <a href="" class="question-delete">Delete</a>
+            </span>
+          </div>
+
+        </div> <!-- .row -->
+
+      </div> <!-- .question-item -->   
+      
+    </div> <!-- .inner-container -->
+
+    <div class="row">
+      <div class="answer-header col-10 col-push-1">
+        <h2>1 Answers</h2>
+      </div>  
+    </div>
+    
+    <div class="inner-container">
+
+      <div class="answer">
+        
+        <div class="row">
+          
+          <div class="answer-status col-2">
+            
+            <div class="vote">
+              <div class="vote-up">
+                <a class="vote-link" href="">▲</a>  
+              </div>
+
+              <div class="vote-counts">
+                <span>0</span>  
+              </div>
+
+              <div class="vote-down">
+                <a class="vote-link" href="">▼</a>
+              </div>
+            </div>
+
+          </div> <!-- .question-status -->
+
+          <div class="answer-content col-10">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat rem quibusdam nihil sequi odit doloremque. Nemo eaque alias repudiandae, consequatur itaque ea pariatur aliquid quasi quo quos, ducimus deserunt eum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, dolorum recusandae dolore delectus expedita, eum eveniet facilis libero rem obcaecati sit ipsam illo doloribus ratione, deleniti tenetur hic aperiam enim.</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, labore quia velit, sint pariatur beatae reiciendis, aperiam sit totam, iste ea sequi. Excepturi, inventore quidem? Rerum mollitia, necessitatibus itaque laudantium?</p>
+          </div>
+
+          <div class="answer-meta">
+            <span>
+              Answered by
+              Name |
+              <a href="edit.php" class="question-edit">Edit</a> |
+              <a href="" class="question-delete">Delete</a>
+            </span>
+          </div>
+
+        </div> <!-- .row -->
+
+      </div> <!-- .answes -->           
+
+    </div> <!-- .inner-container -->
+
+    <div class="inner-container">
+      
+      <div class="row">
+        
+        <div class="answer-form col-10 col-push-2">
+          
+          <h3 class="answer-form-header">Your Answer</h3>
+          
+          <form id="answerForm" action="" method="POST">
+            <div class="form-field">
+              <label for="name">Name</label>
+              <input id="name" type="text">  
+            </div>
+
+            <div class="form-field">
+              <label for="email">Email</label>
+              <input id="email" type="text">  
+            </div>
+            
+            <div class="form-field">
+              <label for="content">Answer</label>
+              <textarea></textarea>
+            </div>
+          </form>
+
+        </div>
+
+      </div>
+
+    </div> <!-- .inner-container -->
+
+  </div> <!-- .outer-container -->
+  
+<?php include( 'template/footer.php' ); ?> 
