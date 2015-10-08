6673ce01e895a28d3e9cf1c2d66e6e27567cb46f
fslprbw <fslprbw@gmail.com>
2015-10-09 06:34:48 +0700

    Ask, HTML + CSS
    Halaman "Pertanyaan", HTML + CSS Standard

---
+++ ask.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ask</title>
+	<link rel="stylesheet" href="ask.css">
+</head>
+<body>
+	<h1 align="Middle"> Simple StackExchange</h1>
+	<br>
+	<h2> What's your question ? </h2>
+	<hr>
+
+	<form action="ask.php">
+		<input class="name" type="text" name="nama" value=""> <br>
+		<input class="email" type="text"  name="email" value""> <br>
+		<input class="topic" type="text" name="topik" value""> <br>
+		<input class="content" type="textarea" name="konten" value""> <br>
+		<input class="submit-button" type="submit" value="Post">
+	</form>	
+
+
+</body>
+</html>
