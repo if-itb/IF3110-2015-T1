e50cc7fead100b83519588234eff938df55cdbc0
MarcoOrlando <13513038@std.stei.itb.ac.id>
2015-10-08 07:10:33 +0700

    Add Database (Question & Answer)

--- WBD_stackexchange/index.php
+++ WBD_stackexchange/index.php
@@ -1,13 +1,40 @@
+<!--
+* Created by PhpStorm.
+* User: Marco Orlando
+* Date: 10/4/2015
+* Time: 8:50 PM
+-->
+
 <!DOCTYPE html>
 <html>
-halo
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Simple StackExchange</title>
+</head>
+
+<body>
+    <div id = "container">
+        <div id = "header">
+            <span id="Judul">Simple StackExchange</span>
+            <span class="Search">
+                <input class="textbox" type="text"/>
+                <input class="searchButton" type="submit" value="Search"/>
+                <p>Cannot find what you are looking for? <a style="color:red">Ask Here Anyway</a>
+            </span>
+        </div>
+    </div>
+    <div id ="body">
+        <h2>Recently Asked Questions</h2>
+        <span class = "questionItem">
+            <!-- isi -->
+        </span>
+    </div>
+</body>
+
+
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Marco Orlando
- * Date: 10/4/2015
- * Time: 8:50 PM
- */
+
 
 ?>
 
