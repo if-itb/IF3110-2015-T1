f0f01906d0206a00b261cb31e413dbdb54b55896
Juan Anton <juan.anton.che@gmail.com>
2015-10-08 23:37:42 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Simple StackExchange </title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<h1>Simple StackExchange</h1>
+		<div id="div1">
+			<FORM>
+			<INPUT TYPE ="SEARCH">
+			<INPUT TYPE ="Submit" VALUE="Search">
+		</FORM>
+		</div>
+		<p id="p1">
+			Cannot find what you are looking for?
+			<a id="a1" href="ask.php">
+				Ask here
+			</a>
+		</p>
+		<br>
+		<div id="div2">
+			<p id="p2">
+				Recently Asked Questions
+			</p>
+		</div>
+	</body>
+</html>
