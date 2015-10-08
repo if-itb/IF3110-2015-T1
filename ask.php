acacd7d502916535d3e3f545faf4a46192629279
Juan Anton <juan.anton.che@gmail.com>
2015-10-08 23:38:32 +0700

    Create ask.php

---
+++ ask.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Simple StackExchange </title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<h1>Simple StackExchange</h1>
+		<br>
+		<div id="div2">
+			<p id="p3">
+				What's your question?
+			</p>
+		</div>
+	</body>
+</html>
