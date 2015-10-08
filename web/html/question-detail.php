87310f51ca15bf6df7738fe6cbf84853fe498553
Irene <irenehuang189@gmail.com>
2015-10-08 21:08:04 +0700

    Add question detail css

--- web/html/question-detail.php
+++ web/html/question-detail.php
@@ -1,7 +1,7 @@
 <!-- Tugas 1 IF3110 Pengembangan Aplikasi Berbasis Web
 Membuat website seperti Stack Exchange
 Author: Irene Wiliudarsan (13513002) -->
-<!-- File: question-detail.php  -->
+<!-- File: answer-question-detail.php  -->
 
 <!DOCTYPE html>
 <html>
@@ -18,39 +18,37 @@ Author: Irene Wiliudarsan (13513002) -->
 
     <div class="content">
       <!-- Question -->
-      <div class="stacked" id="question-detail">
+      <div class="stacked">
         <div class="subtitle">
           The Question topic goes here
         </div>
         <!-- Question Content -->
-        <div>
-          <span class="vote-number">
+        <div class="same-height-row border-bottom">
+          <div class="vote-number">
             <a href="">
-              up<br>
+              <img class="small-icon" src="../img/up.png"/><br>
             </a>
-            (number)<br>
+            <div class="big-number">
+              (number)
+            </div>
             <a href="">
-              down<br>
+              <img class="small-icon" src="../img/down.png"/><br>
             </a>
-          </span>
-          <span class="question-answer-content">
-            The question goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis dapibus nunc, vel vestibulum massa dapibus et. Curabitur quis est metus. Curabitur varius dui ac ex tempor luctus. Vestibulum ut ornare tellus. Integer rhoncus, mauris sed tempus cursus, orci arcu eleifend tortor, quis scelerisque tellus lorem vel neque. Donec ut odio lacinia, ultricies mauris quis, egestas nisl. Suspendisse potenti.
-          </span>
-        </div>
-        <!-- Question Menu -->
-        <div class="right-position">
-          asked by (username) at (datetime)|
-          <span class="edit">
-            <a href="">
+          </div>
+          <div class="right-position">
+            <div class="answer-question-detail">
+              The question goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis dapibus nunc, vel vestibulum massa dapibus et. Curabitur quis est metus. Curabitur varius dui ac ex tempor luctus. Vestibulum ut ornare tellus. Integer rhoncus, mauris sed tempus cursus, orci arcu eleifend tortor, quis scelerisque tellus lorem vel neque. Donec ut odio lacinia, ultricies mauris quis, egestas nisl. Suspendisse potenti.
+            </div>
+            <!-- Asked by -->
+            asked by (username) at (datetime) |
+            <a class="yellow" href="">
               edit
             </a>
-          </span>
-          |
-          <span class="delete">
-            <a href="">
+            |
+            <a class="red" href="">
               delete
             </a>
-          </span>
+          </div>
         </div>
       </div>
       
@@ -59,25 +57,38 @@ Author: Irene Wiliudarsan (13513002) -->
         <div class="subtitle">
           (Number) Answer
         </div>
-        <div>
-          <span class="vote-number">
-            up<br>
-            (number)<br>
-            down
-          </span>
-          <span class="question-answer-content">
-            The answer content goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis dapibus nunc, vel vestibulum massa dapibus et. Curabitur quis est metus. Curabitur varius dui ac ex tempor luctus. Vestibulum ut ornare tellus. Integer rhoncus, mauris sed tempus cursus, orci arcu eleifend tortor, quis scelerisque tellus lorem vel neque. Donec ut odio lacinia, ultricies mauris quis, egestas nisl. Suspendisse potenti.
-          </span>
-
-          <!-- Answer Form -->
-          <h2>Your Answer</h2>
-          <form id="answer-form" action="question-detail.php" method="post">
-            <input class="short-text" name="name" type="text" placeholder="Name">
-            <input class="short-text" name="email" type="text" placeholder="Email">
-            <textarea name="content" placeholder="Content" rows="10" cols="50"></textarea>
-            <input class="button" type="submit" value="Post">
-          </form>
+        <div class="same-height-row border-bottom">
+          <div class="vote-number">
+            <a href="">
+              <img class="small-icon" src="../img/up.png"/><br>
+            </a>
+            <div class="big-number">
+              (number)
+            </div>
+            <a href="">
+              <img class="small-icon" src="../img/down.png"/><br>
+            </a>
+          </div>
+          <div class="right-position">
+            <div class="answer-question-detail">
+              The answer content goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In lobortis dapibus nunc, vel vestibulum massa dapibus et. Curabitur quis est metus. Curabitur varius dui ac ex tempor luctus. Vestibulum ut ornare tellus. Integer rhoncus, mauris sed tempus cursus, orci arcu eleifend tortor, quis scelerisque tellus lorem vel neque. Donec ut odio lacinia, ultricies mauris quis, egestas nisl. Suspendisse potenti.
+            </div>
+            answered by (username) at (datetime)
+          </div>
         </div>
+      </div>
+
+      <!-- Answer Form -->
+      <div class="stacked">
+        <div id="answer-form-title">
+          Your Answer
+        </div>
+        <form class="right" id="answer-form" action="question-detail.php" method="post">
+          <input class="full-length" name="name" type="text" placeholder="Name">
+          <input class="full-length" name="email" type="text" placeholder="Email">
+          <textarea class="full-length" name="content" placeholder="Content" rows="10" cols="50"></textarea>
+          <input class="button" type="submit" value="Post">
+        </form>
       </div>
     </div>
 
