58fa2730c042d0fa5d7237e88b09b50c76974570
miftahulmahfuzh <miftahulmahfuzh@gmail.com>
2015-10-08 20:02:01 +0700

    repairing questions and answers

--- answers/answers.php
+++ answers/answers.php
@@ -6,57 +6,47 @@
 </head>
 <body>
 	<div id="big">Simple StackExchange</div>
-	<?php $conn = new mysqli("localhost","root","","stackoverflow");
-	if($conn->connect_error)
-		die("Connection failed : " . $conn->connect_error);
-	$sql = "SELECT * FROM questions WHERE no=11";
-	$result = mysqli_query($conn, $sql);
-	$row = mysqli_fetch_assoc($result);
-	echo "<div class=\"medium\" id=\"m1\">". $row["question"] ."</div>";
-	echo "<div class=\"medtab\"><table>";
-		echo"<tr>";
-			echo"<td id=\"num\">";
-				echo"<table>";
-					echo"<tr><td>▲</td></tr>";
-					echo"<tr><td>". $row["vote"] ."</td></tr>";
-					echo"<tr><td>▼</td></tr>";
-				echo"</table>";
-			echo"</td>";
-			echo"<td id=\"anscontent\">". $row["content"] ."</td>";
-		echo"</tr>";
-		echo"<tr>";
-			echo"<td id=\"asker\">asked by ". $row["email"] ." at ". $row["time"] ." <a href=\"../questions/editquestions.php\">edit</a> | delete</td>";
-		echo"</tr>";
-	echo"</table></div>"; 
-	$sum = "SELECT COUNT(*) AS SHIT FROM answers WHERE question_no=11";
-	$ans = mysqli_query($conn, $sum);
-	while ($row=$ans->fetch_assoc()) {
-	echo"<div class=\"medium\" id=\"m1\">".$row['SHIT'] ." Answer</div>"; }
-	$sql = "SELECT * FROM answers WHERE question_no=11";
-	$result = mysqli_query($conn, $sql);
-	while($row = mysqli_fetch_assoc($result)) {
-	echo"<div class=\"medtab\"><table>";
-		echo"<tr>";
-			echo"<td id=\"num\">";
-				echo"<table>";
-					echo"<tr><td>▲</td></tr>";
-					echo"<tr><td>". $row["vote"] ."</td></tr> ";
-					echo"<tr><td>▼</td></tr>";
-				echo"</table>";
-			echo"</td>";
-			echo"<td id=\"anscontent\">". $row["content"] ."</td>";
-		echo"</tr>";
-		echo"<tr>";
-			echo"<td id=\"asker\">answered by ". $row["email"] ." at ". $row["time"] ."</td>";
-		} $conn->close();
-		?></tr>
+	<div  class="mediumbaru">
+	
+	<div id=\"m1\">Qeustion</div>
+	<div><table class=\"medtab\">
+		<tr>
+			<td id=\"num\">
+				<table>
+					<tr><td>▲</td></tr>
+					<tr><td>0</td></tr>
+					<tr><td>▼</td></tr>
+				</table>
+			</td>
+			<td id=\"anscontent\">Question content</td>
+		</tr>
+		<tr>
+			<td id=\"asker\">asked by mahfuzh74 at Saturday <a href=\"../questions/editquestions.php\">edit</a> | delete</td>
+		</tr>
+	</table></div> 
+	
+	<div><table class=\"medtab\">
+		<tr>
+			<td id=\"num\">
+				<table>
+					<tr><td>▲</td></tr>
+					<tr><td>0</td></tr> 
+					<tr><td>▼</td></tr>
+				</table>
+			</td>
+			<td id=\"anscontent\">Answer content</td>
+		</tr>
+		<tr>
+			<td id=\"asker\">answered by Nana at Saturday</td>
+		</tr>
 	</table></div>
-	<div class="medium" id="m2">Your Answer</div>
+	<div id="m2">Your Answer</div>
 	<form name="makequestion" method="post" action="sendanswers.php">
 		 <input type="text" name="name" placeholder="Name" class="medium">
 		 <input type="email" name="email" placeholder="Email" class="medium">
 		 <textarea type="text" name="content" placeholder="Content" class="medium" id="content"></textarea> 
 		 <input type="submit" value="Post" id="button">
 	 </form>
+	 </div>
 </body>
 </html>
--- home/homepage.php
+++ home/homepage.php
@@ -5,7 +5,40 @@
 	<title>Home</title>
 </head>
 <body>
+	<div class="mediumbaru">
 	<div id="big">Simple StackExchange</div>
-	
+	<div class="input">
+		<input class="input1" type="text">
+		<input class="input2" type="submit" value="Search" id="search">
+	</div>
+	<strong> <div id="m4">Cannot find what you're looking for? 
+	<a href="../questions/questions.php"> Ask here</a></div> </strong>
+	<div id="m1">Recently Asked Questions</div>
+	<div>  <?php $conn = new mysqli("localhost","root","","stackoverflow");
+	if($conn->connect_error)
+		die("Connection failed : ". $conn->connect_error);
+	$sql = "SELECT * FROM questions";
+	$ans = mysqli_query($conn, $sql);
+	while($row=$ans->fetch_assoc()) {
+	echo "<div class=\"div1\">
+			<div class=\"div2\">
+			<div class=\"div4\">";
+				echo "<span>".$row['vote']."</span>";
+				echo "<span>Vote</span>";	
+			echo "</div>";
+			echo "<div class=\"div5\">";
+				echo "<span>".$row['vote']."</span>";
+				echo "<span>Answer</span>";
+			echo "</div>";
+		echo "</div>";
+		echo "<div class=\"div3\">";
+			echo "<div class=\"div6\">".$row['question']."</div>";
+			echo "<div class=\"div7\">Asked by ".$row['name']." at ".$row['time']." | edit | delete</div>";
+		echo "</div>";
+	echo "</div>";
+	} $conn->close();
+	?> 
+	</div>
+	</div>
 </body>
 </html>
