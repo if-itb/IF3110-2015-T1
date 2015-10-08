2a3717b010bae64c3288da76f4bee89ff6defc85
MarcoOrlando <13513038@std.stei.itb.ac.id>
2015-10-09 05:32:11 +0700

    Index: Fetch question from database

--- WBD_stackexchange/index.php
+++ WBD_stackexchange/index.php
@@ -19,23 +19,59 @@
             <span class="Search">
                 <input class="textbox" type="text"/>
                 <input class="searchButton" type="submit" value="Search"/>
-                <p>Cannot find what you are looking for? <a style="color:red">Ask Here Anyway</a>
+                <p>Cannot find what you are looking for? <a style="color:red">Ask here</a>
             </span>
         </div>
+        <div id ="body">
+            <h2>Recently Asked Questions</h2>
+            <?php
+                // Connect to database server
+                mysql_connect("localhost", "root", "") or die (mysql_error ());
+
+                // Select database
+                mysql_select_db("WBD1") or die(mysql_error());
+                // SQL query
+                $strSQL = "SELECT * FROM Questions";
+
+                // Execute the query (the recordset $rs contains the result)
+                $rs = mysql_query($strSQL);
+
+                // Loop the recordset $rs
+                // Each row will be made into an array ($row) using mysql_fetch_array
+                while($row = mysql_fetch_array($rs)) {
+
+                    // Write the value of the column FirstName (which is now in the array $row)
+                    $name = $row['name'];
+                    $title = $row['title'];
+                    $vote = $row['vote'];
+                    $questionId = $row['questionId'];
+                    $query_2="SELECT * FROM Answers where question_id=$questionId";
+                    $query_2_result= mysql_query($query_2);
+                    $num_answers = mysql_num_rows($query_2_result);
+
+                    echo "<div class = 'questionItem'>";
+                        echo"<div class = questionLeft>";
+                            echo"<span class = 'votes_number'>$vote<br>Votes</span>";
+                            echo"<span class = 'Answers_number'>$num_answers<br>Answers</span>";
+                        echo"</div>";
+
+                        echo"<div class= questionMid>";
+                            echo"<a class ='title'>$title<br></a>";
+                        echo"</div>";
+
+                        echo"<div class= questionRight>";
+                            echo"asked by <span class='questionSubject'>$name</span>|<a class='editQuestion'>edit</a>|<a class='deleteQuestion'>delete</a>";
+                        echo"</div>";
+                    echo"</div>";
+                }
+
+                // Close the database connection
+                mysql_close();
+            ?>
+
+        </div>
+
     </div>
-    <div id ="body">
-        <h2>Recently Asked Questions</h2>
-        <span class = "questionItem">
-            <!-- isi -->
-        </span>
-    </div>
+
 </body>
-
-
-
-<?php
-
-
-?>
-
 </html>
