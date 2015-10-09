8f37efc150a186af04ed632c4e5520b0604264c4
Mochamad Try Yulyanto <cintaparent@gmail.com>
2015-10-09 09:32:58 -0700

    basic home

---
+++ home.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="questionStyle.css"/>
+		<title>Home</title>
+	</head>
+
+<body>
+<div class="container">
+<div id="header">Simple StackExchange</div>
+<div>
+<form>
+	<input class="kotaksearch" type="text" name="search">
+	<input class="search" type="submit" value="Search">
+</form>
+</div>
+
+<div class="ask"> Cannot find what you are looking for ? <a class="link" href="#"> Ask Here </a>  </div>
+
+<div>
+	<div class="header2" id="main3"> Recently Asked Questions </div>
+	<div>
+		<div>
+			<div class="vote">
+				 <span class="vote-counts">0</span>
+				 <div class="vote-title">Vote</div>
+			</div>
+
+			<div class="answer">
+				 <div class="answer-counts">0</div>
+				 <span class="answer-title">Answer</span>
+			</div>
+			
+		</div>
+		<div>
+			<div class="topic"> <a href="#"> The question topic goes here </a> </div>
+			<p class="asked"> asked by name | <a href="#"> edit </a> | <a href="#"> delete </a>  </p>
+		</div>
+		<!--<div>
+			<p class="asked"> asked by name | <a href="#"> edit </a> | <a href="#"> delete </a>  </p>
+		</div>
+
+	</div>
+
+	<div>
+		<div>
+			<div>
+				 0 
+			</div>
+			<div>
+				 Vote 
+			</div> 
+		</div>
+
+		<div>
+			<div>
+				 0 
+			</div>
+			<div>
+				 Answer 
+			</div> 
+		</div>
+		<div>
+			<p> <a href="#"> The question topic goes here </a> </p>
+		</div>
+		<div>
+			<p> asked by name | <a href="#"> edit </a> | <a href="#"> delete </a>  </p>
+		</div>
+
+	</div>
+</div>
+</div>-->
+<!-- <p>This is a paragraph.</p> -->
+
+</body>
+</html>
