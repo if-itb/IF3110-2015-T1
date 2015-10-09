4052b65a1a83deb6d194ed204d9d9d46eafef677
Ivan Andrianto <andrianto.ivan@yahoo.co.id>
2015-10-09 14:54:22 +0700

    add functions for validateAnswer and vote

--- assets/script.js
+++ assets/script.js
@@ -38,3 +38,75 @@ function chkValidity(){
 	
 	return true;
 }
+
+function chkValidityAnswer(){
+				
+	//nama
+	var txtName = document.getElementById('name').value;
+	if(txtName.length<=0){
+		alert("Nama tidak boleh kosong");
+		return false;
+	}
+
+	//email
+	var txtEmail = document.getElementById('email').value;
+	if(txtEmail.length<=0){
+		alert("Email tidak boleh kosong");
+		return false;
+	}
+	//validate email format
+	var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	var emailValid = re.test(txtEmail);
+	if(!emailValid){
+		alert("Email tidak valid");
+		return false;
+	}
+
+	//content
+	var txtContent = document.getElementById('content').value;
+	if(txtContent.length<=0){
+		alert("Content tidak boleh kosong");
+		return false;
+	}
+
+	return true;
+}
+
+function vote(upDown, type, id) {
+	var xmlhttp;
+	if (window.XMLHttpRequest){
+		xmlhttp=new XMLHttpRequest();
+	} else {
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	
+	if(type == 'q'){	
+		xmlhttp.onreadystatechange = function()
+		{
+			if(xmlhttp.readyState == 4)
+			{
+				document.getElementById("q-"+id).innerHTML=xmlhttp.responseText;
+			}
+		}
+		if(upDown == 'up'){
+			xmlhttp.open("POST","functions.php?f=voteQuestionUp&id=" + id,true);
+		} else {
+			xmlhttp.open("POST","functions.php?f=voteQuestionDown&id=" + id,true);
+		}
+		xmlhttp.send();
+	} else { //answer
+		xmlhttp.onreadystatechange = function()
+		{
+			if(xmlhttp.readyState == 4)
+			{
+				document.getElementById("a-"+id).innerHTML=xmlhttp.responseText;
+			}
+		}
+		if(upDown == 'up'){
+			xmlhttp.open("POST","functions.php?f=voteAnswerUp&id=" + id,true);
+		} else {
+			xmlhttp.open("POST","functions.php?f=voteAnswerDown&id=" + id,true);
+		}
+		xmlhttp.send();
+		}
+}
