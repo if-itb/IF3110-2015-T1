718c0132f2cb01e53cf9dd651d00603145d95dcc
Ignatius Alriana Haryadi Moel <alrianatcm@gmail.com>
2015-10-09 17:16:37 +0700

    Database for answer tables done

--- dbact.php
+++ dbact.php
@@ -17,7 +17,6 @@
 			VALUES ('$Name', '$Email', '$Topic', '$content')";
 		
 		$res = mysqli_query($db, $qry);
-		echo "INSERT";
 		return $res;
 		
 	}
@@ -28,7 +27,6 @@
 		$qry = "UPDATE question SET Name='$Name', Email='$Email', Topic='$Topic', Content='$content' WHERE id_q=$id_q";
 		
 		$res = mysqli_query($db, $qry);
-		echo "EDIT'";
 		return $res;
 		
 	}
@@ -58,6 +56,29 @@
 		$qry = "DELETE FROM question WHERE id_q=$id_q";
 
 		$res = mysqli_query($db, $qry);
+		return $res;
+	}
+
+	function addAnswer($id, $Name, $Email, $content){
+		global $db;
+
+		$qry = "INSERT INTO answer (id_q, Name, Email, Content)
+			VALUES ('$id', '$Name', '$Email', '$content')";
+		
+		$res = mysqli_query($db, $qry);
+		$qry2 = "UPDATE question SET nAns=nAns+1 WHERE id_q=$id" ;
+		$res = mysqli_query($db, $qry2);
+		return $res;
+		
+	}
+
+	function getAnsfor($id_q){
+		global $db;
+
+		$qry = "SELECT * FROM answer WHERE id_q=$id_q ORDER BY created_date DESC" ;
+		
+		$res = mysqli_query($db, $qry);
+
 		return $res;
 	}
 ?>
