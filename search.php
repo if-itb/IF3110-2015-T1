a5312aad5a9b0f27482b636edd81f55855fbb50d
Mochamad Try Yulyanto <cintaparent@gmail.com>
2015-10-09 09:33:37 -0700

    basic search

---
+++ search.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="questionStyle.css"/>
+		<title>Question</title>
+	</head>
+
+<body>
+<div class="container">
+<div id="header"> Simple StackExchange </div>
+<div class="header2" id="main2">What's your question?</div>
+
+<!--<h1>Simple StackExchange</h1>-->
+
+<div>
+	<div>
+		<form>
+			<input type="text" name="name" placeholder="Name" class="header3" > 
+		</form>
+	</div>
+	<div>
+		<form>
+			<input type="text" name="email" placeholder="Email" class="header3" > 
+		</form>
+	</div>
+	<div>
+		<form>
+			<input type="text" name="question" placeholder="Question Topic" class="header3" > 
+		</form>
+	</div>
+	<div>
+		<form>
+			<textarea type="text" name="content" placeholder="Content" class="header3" id="content"></textarea> 
+		</form>
+	</div>
+	<div>
+	<input type="submit" value="Post" id="button">
+	<!--a href="#"><div id="button">Post</div></a>-->
+</div>
+
+</div>
+
+
+
+
+</body>
+</html>
