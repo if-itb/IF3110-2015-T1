767b1e9be1bdcc9f139f7a3eac220d05e12bfa3f
Steven Andianto <stevenandianto@gmail.com>
2015-10-10 02:31:04 +0700

    memiliki conditional penanganan edit dan new question

--- wbd.php
+++ wbd.php
@@ -7,9 +7,10 @@
 <body>
 	<h1>Simple StackExchange</h1>
 	<div align="center" class="container-4">
-	<form name:"SearchBox" method="post">
+	<form name:"SearchBox">
 	<input type="text" id="search" name="SearchBox" placeholder="Search here...">
-	<input type="submit" id="submit" value="Search" name="SearchButton">		
+	<input type="submit" id="submit" value="Search" name="SearchButton">	
+	</form>	
 	</div>
 	<p id="judul2">Cannot find what you are looking for? <a href="wbd2.php" id="yellow">Ask here</a></p>
 	<br>
@@ -18,7 +19,7 @@
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
-		$dbname = "WBD";
+		$dbname = "wbd";
 
 		// Create connection
 		$conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -27,10 +28,20 @@
 			die("Connection failed: " . mysqli_connect_error());
 		}
 		if($_SERVER['REQUEST_METHOD'] == 'POST'){
+		$sql = NULL;
+		if ($_POST['id']=='') {
 		$sql = "INSERT INTO question (nama, email, topic, content, date, vote)
-		VALUES ('$_POST[InputName]', '$_POST[InputEmail]', '$_POST[InputQuestionTopic]', '$_POST[InputContent]',NOW()),0";
-		$results = mysqli_query($conn,$sql);
-		$edit = "Tambah";
+		VALUES ('$_POST[InputName]', '$_POST[InputEmail]', '$_POST[InputQuestionTopic]', '$_POST[InputContent]',NOW(),0)";
+		}
+		else {$sql = "UPDATE question
+				SET
+					nama='$_POST[InputName]',
+					email='$_POST[InputEmail]',
+					topic='$_POST[InputQuestionTopic]',
+					content='$_POST[InputContent]'
+				WHERE
+					id = $_POST[id]";
+		}
 		if(mysqli_query($conn,$sql))
 		{ 
 		echo "New record created successfully!";
@@ -48,7 +59,7 @@
 			echo	'<td id="td1">'.$row['vote'].'<br>Votes</td>';
 			echo	'<td id="td1">0<br>Answers</td>';
 			echo 	'<td id="td2">'.$row['topic'].'</td>';
-			echo	'<td id="td3">asked by <a href="wbd2.php" id="blue">'.$row['nama'].'</a> | <a href="wbd2.php" id="yellow">edit</a> | <a href="wbd2.php" id="red">delete</a></td>';
+			echo	'<td id="td3">asked by <a href="wbd2.php" id="blue">'.$row['nama'].'</a> | <a href="wbd2.php?id='.$row['id'].'" id="yellow">edit</a> | <a href="wbd2.php" id="red">delete</a></td>';
 			echo	'</tr>';
 			echo	'</table>';
 			echo	$row['id'];
@@ -56,7 +67,7 @@
 			}
 		mysqli_close($conn);
 		?> 
-		</form>
+		
 </body>
 
 </html>
