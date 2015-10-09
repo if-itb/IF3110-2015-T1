4a29d8ec4392a819dfe0b185876c60fda71a9ac8
Steven Andianto <stevenandianto@gmail.com>
2015-10-09 12:50:24 +0700

    bikin front end php untuk home page
    belum ada backend hanya prototype

---
+++ wbd.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="wbd.css">
+	<title>13513018_Steven Andianto</title>
+</head>
+<body>
+	<h1>Simple StackExchange</h1>
+	<div align="center">
+	<form name:"SearchBox">
+	<input type="text" id="searchbox" name="SearchBox">
+	<input type="submit" id="submit" value="Search" name="SearchButton">		
+	</form>
+	</div>
+	<p id="judul2">Cannot find what you are looking for? <a href="wbd2.php" style="color:#FFA000">Ask here</a></p>
+	<br>
+	<p id="judul">Recently Asked Questions</p>
+	<div class="content">
+	<hr>
+	<p align="center">The question topic goes here</p>
+		<div>
+		 0
+			<div>
+			0
+			</div>
+		</div>
+	<p align="left">votes  answer</p>
+	</div>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
