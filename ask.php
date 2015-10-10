ee97c3cbae666cd8532ed26a816c690a407efe36
Octavianus Marcel Harjono <octavianusmarcel12@gmail.com>
2015-10-10 11:55:41 +0700

    initial commit
    add ask.php
    add index.php
    add style.css

---
+++ ask.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Ask </title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body>
+		<h1>Simple StackExchange</h1>
+		
+		<h2>What's your question?</h3>
+		<br>
+		
+		<form>
+			<input class="textBox" type="text" placeholder="Name">
+			<br><br>
+			<input class="textBox" type="text" placeholder="Email">
+			<br><br>
+			<input class="textBox" type="text" placeholder="Question Topic">
+			<br><br>
+			<textarea placeholder="Content"></textarea>
+			<br><br>
+			<div class="right">
+				<input class="postButton" type="Submit" value="Post">
+			</div>
+		</form>
+	</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Index </title>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body>
+		<h1>Simple StackExchange</h1>
+		<form action="Search.php">
+			<input class="searchBox" type="text" name="questions">
+			<input class="submitButton" type="submit" value="Search">
+		</form>
+		<p>
+			Cannot find what you are looking for? <a href="ask.php">Ask here</a>
+			<br>
+		</p>
+		
+		<h3>Recently Asked Question</h3>
+		<br>
+		
+		<div class="question">
+			
+		</div>
+	</body>
+</html>
