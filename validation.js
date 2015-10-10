7ca63cd44eb537f4774ea8364eab21cc15e42b79
William Sentosa <williamsentosa.sentosa@gmail.com>
2015-10-10 18:51:58 +0700

    add validation for checking form
    checking form which consist of name, email, topic, and content

---
+++ validation.js
@@ -0,0 +1,32 @@
+function isNotValid(x) {
+	return x==null || x=="" ;
+}
+
+function isEmailvalid(email) {
+	var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(email);
+}
+
+function validateForm() {
+	var name = document.forms["inputForm"]["asker_name"].value;
+	var email = document.forms["inputForm"]["asker_email"].value;
+	var topic = document.forms["inputForm"]["question_topic"].value;
+	var content = document.forms["inputForm"]["content"].value;
+	if(isNotValid(name)) {
+		alert("Name must be filled out");
+		return false;
+	} else if(isNotValid(email)) {
+		alert("Email must be filled out");
+		return false;
+	} else if(!isEmailvalid(email)) {
+		alert("Email is not valid");
+		return false;
+	} else if(isNotValid(topic)) {
+		alert("Question Topic must be filled out");
+		return false;
+	} else if(isNotValid(content)) {
+		alert("Content must be filled out");
+		return false;
+	} 
+	return true;
+}
