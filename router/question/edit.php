0f7fd77c12c5d37bb3d7c8fc2f329cf8d985e89d
elvan-owen <elvanowen95@gmail.com>
2015-10-10 15:47:48 +0700

    Added router for editing questions page

---
+++ router/question/edit.php
@@ -0,0 +1,50 @@
+<?php
+
+    namespace routes\question;
+
+    if(!isset($_GET["id"])) {
+      exit();
+    }
+
+    $question_id = $_GET["id"];
+
+    define("ROOT", $_SERVER['DOCUMENT_ROOT'] ."/../");
+
+    require_once(ROOT . '/models/question/question.php');
+    require_once(ROOT . '/utils/mysql/connection.php');
+    require_once(ROOT . '/utils/response/render.php');
+
+    use utils\mysql;
+    use utils\response;
+    use models\question;
+
+    $conn = mysql\connect();
+
+    // Get Question's Details
+    $sql = "
+        SELECT 
+            id, name, email, topic, content
+        FROM 
+            questions
+        WHERE
+            id = $question_id";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $question = new question\QuestionModel($row);
+        }
+    }
+
+    if ($question == null){
+        exit();
+    }
+
+    $params = array("question" => $question);
+
+    mysql\close($conn);
+
+    
+    response\render("ask/index.php", $params);
+?>
