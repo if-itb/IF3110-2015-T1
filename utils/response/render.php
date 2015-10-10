a534dd74a3689caddbb02ae394300da4f1eb28a4
elvan-owen <elvanowen95@gmail.com>
2015-10-10 15:28:06 +0700

    Add php render and json response util function

---
+++ utils/response/render.php
@@ -0,0 +1,21 @@
+<?php
+    
+    namespace utils\response;
+
+    function render($filename, $data){
+        $path = ROOT . 'views/' . $filename;
+        include $path;
+    };
+
+    function returnMessageJSON($status, $message){
+        http_response_code($status);
+        $arr = array('message' => $message);
+
+        echo json_encode($arr);
+    };
+
+    function returnDataJSON($status, $data){
+        http_response_code($status);
+        echo json_encode($data);
+    };
+?>
