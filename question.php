8c8def80cdeb8c75806fe7bda0395f77d213a896
William Sentosa <williamsentosa.sentosa@gmail.com>
2015-10-10 18:51:09 +0700

    form worked
    adding function for adding data to database

--- question.php
+++ question.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="style.css">
+		<script type="text/javascript" src="validation.js"></script>
 		<title>Stack Exchange</title>
 	</head>
 	<body>
@@ -9,17 +10,17 @@
 		<br>
 		<h2 class="header">What's your question ?</h2>
 		<br>
-		<form>
+		<form name = "inputForm" onsubmit="return validateForm()" action="post.php" method="post" >
 			<div id = "name_box">
 				<input type="text" name="asker_name" placeholder="Name"><br>
 				<input type="text" name="asker_email" placeholder="Email"><br>
-				<input type="text" name="question_type" placeholder="Question Topic"><br>
+				<input type="text" name="question_topic" placeholder="Question Topic"><br>
 			</div>
 			<div>
-				<textarea placeholder="Content"></textarea><br><br>
+				<textarea name="content" placeholder="Content"></textarea><br><br>
 			</div>
 			<div id = "post_button">
-				<input type="submit" value="Post">
+				<input type="submit" value="Post" onclick="location.href='index.php'">
 			</div>
 		</form>
 	</body>
