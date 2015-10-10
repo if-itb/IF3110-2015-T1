4bea61f07a4fb009c3fadad649337ff3d42c3acf
William Sentosa <williamsentosa.sentosa@gmail.com>
2015-10-10 18:49:19 +0700

    index updated
    implement header and footer, add link to question.php

--- index.php
+++ index.php
@@ -12,27 +12,40 @@
 				<input type="submit" name="search_button" value="Search">
 			</div>
 		</form>
-		<p class="ask">Cannot find what you are looking for? <a class="ask" href=#>Ask here</a></p>
+		<p class="ask">Cannot find what you are looking for? <a class="ask" href="question.php">Ask here</a></p>
 		<br>
 		<p>Recently Asked Question</p>
-		<div class="list">
-			<div class="vote">0<br>Votes</div>
-			<div class="answer_count">0<br>Answers</div>
-			<div class="question_topic">The question topic goes here</div>
-			<div class="asked_by">asked by name | edit | delete</div>
-		</div>
-		<div class="list">
-			<div class="vote">0<br>Votes</div>
-			<div class="answer_count">0<br>Answers</div>
-			<div class="question_topic">The question topic goes here</div>
-			<div class="asked_by">asked by name | edit | delete</div>
-		</div>
-		<div class="list">
-			<div class="vote">0<br>Votes</div>
-			<div class="answer_count">0<br>Answers</div>
-			<div class="question_topic">The question topic goes here</div>
-			<div class="asked_by">asked by name | edit | delete</div>
-		</div>
+		
+		<?php
+		include 'header.php';
+		$conn = connectToDatabase();
+		?>
+		
+		<?php
+		// Get all data
+		$sql = "SELECT asker_name, question_topic, question_vote FROM question";
+		$result = mysqli_query($conn, $sql);
+		
+		if (mysqli_num_rows($result) > 0) {
+			// output data
+			while($row = mysqli_fetch_assoc($result)) {
+				echo "<div class='list'>";
+				echo "<div class='vote'>".$row["question_vote"]."<br>Votes</div>";
+				echo "<div class='answer_count'>0<br>Answers</div>";
+				echo "<div class='question_topic'>".$row["question_topic"]."</div>";
+				echo "<div class='asked_by'>asked by ".$row["asker_name"]." | edit | delete</div>";
+				echo "</div>";
+			}
+		} else {
+			echo "<p>No Question available, be the first person to ask a question</p>";
+		}
+		?>
+		
+		<?php
+		include 'footer.php';
+		closeDatabase($conn);
+		?>
+		
 	</body>
 </html>
 
