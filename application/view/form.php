fbf6390675c727be0a0179eea588586ad87dc7c8
Rahman Adianto <rahman.adianto@gmail.com>
2015-10-12 00:13:36 +0700

    Add form view to create new thread

---
+++ application/view/form.php
@@ -0,0 +1,14 @@
+<div id="thread-new-title">
+	<h2>What's your question</h2>
+</div>
+<hr>
+
+<div class="form">
+	<form action="">
+		<input type="text" name="user_name" id="user_name" placeholder="Name" autofocus>
+		<input type="email" name="user_email" id="user_email" placeholder="Email">
+		<input type="text" name="thread_topic" id="thread_topic" placeholder="Question Type">
+		<textarea name="thread_content" id="thread_content" rows="10" placeholder="Content"></textarea>
+		<input type="submit" id="submit" value="Post">
+	</form>
+</div>
