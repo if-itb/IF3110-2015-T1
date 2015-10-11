c0f96642a4dedef2c0ec5bb0d8628822d21ef6e2
deredet <lucky.cahyadi@yahoo.com>
2015-10-11 12:40:28 +0700

    add Validate Answer

--- Validate.js
+++ Validate.js
@@ -27,6 +27,31 @@ function validateForm(){
 	}
 }
 
+function validateAnswerForm(){
+	var a = document.forms["aForm"]["name"].value;
+	var b = document.forms["aForm"]["email"].value;
+	var c = document.forms["aForm"]["content"].value;
+	
+	var x="";
+	if(a==null || a==""){
+		x+="Name tidak boleh kosong\n";
+	}
+	if(b==null || b==""){
+		x+="Email tidak boleh kosong\n";
+	}else{
+		if(!validateEmail(b)){
+			x+="Email harus sesuai dengan format\n"
+		}
+	}
+	if(c==null || c==""){
+		x+="Content tidak boleh kosong\n";
+	}
+	if (x!=""){
+		alert(x);
+		return false;
+	}
+}
+
 function validateEmail(email) {
     var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
     return re.test(email);
