9eaf07edd28cf9affe76a16c5246403d278f4a0a
dEUS00 <deus.caj@gmail.com>
2015-10-11 15:14:30 +0700

    Tambah fungsi edit

--- create.php
+++ create.php
@@ -7,6 +7,26 @@
 </head>
 
 <body>
+	<?php
+		$conn = mysql_connect('localhost', 'root', '08161955342');
+		mysql_select_db('simplestackexchange', $conn);
+
+		if (isset($_GET['question_id'])) {
+			$question_id = $_GET['question_id'];
+			$type = 'update';
+		}
+		else {
+			$question_id = '';
+			$type = 'ask';
+		}
+
+		$sql = "SELECT * FROM question where id=$question_id";
+		$data = mysql_fetch_assoc(mysql_query($sql));
+		$data['name'] = !isset($data['name']) ? '' : $data['name'];
+		$data['email'] = !isset($data['email']) ? '' : $data['email'];
+		$data['topic'] = !isset($data['topic']) ? '' : $data['topic'];
+		$data['content'] = !isset($data['content']) ? '' : $data['content'];
+	?>
 	<h1 align="center">
 		Simple StackExchange
 	</h1>
@@ -17,16 +37,19 @@
 	</h2>
 	<hr width="75%" align="center">
 	<br>
-	<form action="submit_question.php" method="POST">
-		<input type="text" name="name" value="Name" onfocus="if (this.value=='Name') this.value='';">
+	<form action="home.php" method="POST">
+		<input type="text" name="name" value="<?php echo $data['name']; ?>" placeholder="Name">
 		<br>
-		<input type="text" name="email" value="Email" onfocus="if (this.value=='Email') this.value='';">
+		<input type="text" name="email" value="<?php echo $data['email']; ?>" placeholder="Email">
 		<br>
-		<input type="text" name="topic" value="Question Topic" onfocus="if (this.value=='Question Topic') this.value='';">
+		<input type="text" name="topic" value="<?php echo $data['topic']; ?>" placeholder="Question Topic">
 		<br>
-		<input type="text" name="content" value="Content" onfocus="if (this.value=='Content') this.value='';">
+		<input type="text" name="content" value="<?php echo $data['content']; ?>" placeholder="Content">
 		<br>
+		<input type="hidden" name="type" value="<?php echo $type; ?>">
+		<input type="hidden" name="question_id" value="<?php echo $question_id; ?>">
 		<input type="submit" name="post" value="Post">
 	</form>
+	<?php mysql_close($conn); ?>
 </body>
 </html>
