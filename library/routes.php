1271e4f295d756dba496c1f7a649361bdfedf842
Rahman Adianto <rahman.adianto@gmail.com>
2015-10-11 23:46:42 +0700

    Add route ro handle controller calling process

---
+++ library/routes.php
@@ -0,0 +1,36 @@
+<?php 
+	function call ($controller, $action, $query) {
+		require_once(ROOT . DS . 'application' . DS . 'controller'. DS . 'class.' . $controller . 'controller.php');
+
+		switch ($controller) {
+			case 'thread' :
+				require_once(ROOT. DS . 'application' . DS . 'model'. DS . 'class.thread.php');
+				$controller = new ThreadController();
+			break;
+			case 'answer' :
+				require_once(ROOT. DS . 'application' . DS . 'model'. DS . 'class.answer.php');
+				$controller = new AnswerController();
+			break;
+		}
+
+		if ($query == '')
+			$controller->{ $action }();
+		else
+			$controller->{ $action }($query);
+	}
+
+	// Need for validation (belum lengkap)
+	$controllers = array('thread' => ['home', 'detail', 'create', 'error']);
+
+	if (array_key_exists($controller, $controllers)) {
+		if (in_array($action, $controllers[$controller])) {
+			call($controller, $action, $queryString);
+		}
+		else {
+			call('thread', 'error', '');
+		}
+	}
+	else {
+		call('thread', 'error', '');
+	}	
+?>
