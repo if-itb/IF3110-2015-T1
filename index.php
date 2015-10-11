c2f1a616ef4fea8c9a6e8ba14ebee3513fb1d459
William Sentosa <williamsentosa.sentosa@gmail.com>
2015-10-11 18:28:59 +0700

    votes and answers count is implemented

--- index.php
+++ index.php
@@ -24,17 +24,27 @@
 		
 		<?php
 		// Get all data
-		$sql = "SELECT question_id, asker_name, question_topic, question_vote FROM question";
+		$sql = "SELECT question_id, asker_name, question_topic, question_content, question_vote FROM question";
 		$result = mysqli_query($conn, $sql);
 		
 		if (mysqli_num_rows($result) > 0) {
 			// output data
 			while($row = mysqli_fetch_assoc($result)) {
+				// Get answers count
+				$sql = "Select count(answer_id) As count From answer Where question_id =".$row["question_id"];
+				$count = mysqli_fetch_assoc(mysqli_query($conn, $sql));
 				echo "<div class='list'>";
 				echo "<div class='vote'>".$row["question_vote"]."<br>Votes</div>";
-				echo "<div class='answer_count'>0<br>Answers</div>";
-				echo "<div class='question_topic'><a href='detail.php?id=".$row["question_id"]."'>".$row["question_topic"]."</a></div>";
-				echo "<div class='asked_by'>asked by <span class='name'>".$row["asker_name"]."</span> | <a href='#' class='edit'>edit</a> | <a href='#' class='delete'>delete</a></div>";
+				echo "<div class='answer_count'>".$count["count"]."<br>Answers</div>";
+				if(strlen($row["question_content"]) >150) {
+					echo "<div class='question'><a class='question_topic' href='detail.php?id=".$row["question_id"]."'>".$row["question_topic"]."</a><br><br>"
+					.substr($row["question_content"],0,149).".....</div>";
+				} else {
+					echo "<div class='question'><a class='question_topic' href='detail.php?id=".$row["question_id"]."'>".$row["question_topic"]."</a><br><br>".$row["question_content"]."</div>";
+				}
+				echo "<div class='asked_by'>asked by <span class='name'>".$row["asker_name"]."</span> | 
+				<a href='question.php?id=".$row["question_id"]."' class='edit'>edit</a> | 
+				<a onclick='return confirmDelete()' href='delete.php?id=".$row["question_id"]."' class='delete'>delete</a></div>";
 				echo "</div>";
 			}
 		} else {
