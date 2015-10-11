1e1faf6d40a33cdfdeb4d6784b28078efd151979
Julio Savigny <jsavigny@gmail.com>
2015-10-11 23:20:53 +0700

    add redirect into question page after posting a question

--- insertquestion.php
+++ insertquestion.php
@@ -8,8 +8,12 @@
 		$content = $_POST['content'];
 		$sql = "INSERT INTO question(name,email,qtopic,content) VALUES ('$name','$email','$qtopic','$content')";
 		$conn->query($sql);
+		$sql = "SELECT id FROM question WHERE name='$name' AND email='$email' AND qtopic='$qtopic' and content='$content'";
+		$result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+		$id = $row['id'];
 		$conn->close();
 	}
-	header("Location: index.php"); 
+	header("Location: question.php?id=$id"); 
 	exit;
 ?>
