10757ceb612dd83d27d4ce68be4f0b74c71b1575
Juan Anton <juan.anton.che@gmail.com>
2015-10-11 15:30:37 +0700

    Update the page style
    Complete with simple edit implementation

--- index.php
+++ index.php
@@ -34,11 +34,25 @@
 		<div id="div3">
 			<?php foreach ($questions as $question) : ?> 
 			<div id="div4">
-				<?php echo $question['Name'] ?>
-				<br>
-				<?php echo $question['Content'] ?>
-				<br>
-				<br>
+				<div id="idx_vote">
+					<div> <?php echo $question['Vote'] ?> </div>
+					<div> Votes </div>
+				</div>
+				<div id="idx_answer">
+					<div> <?php echo 0 ?> </div> 
+					<div> Answer </div>
+				</div>
+				<div id="idx_title">
+					<?php echo $question['Title'] ?>
+				</div>
+				<div id="idx_content">
+					<?php echo $question['Content'] ?>
+				</div>
+				<div id="div5">
+					<p>
+						asked by <span id="idx_name"> <?php echo $question['Name'] ?> </span> | <span id="idx_edit"> <a id="a2" href="ask.php?q_id=<?php echo $question['Q_ID'] ?>"> edit </a> </span>
+					</p>
+				</div>
 			</div>
 			<?php endforeach; ?>
 		</div>
