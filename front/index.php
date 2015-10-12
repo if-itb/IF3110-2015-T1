39925a11701f039abf48a649cf7f4a20b9233f7c
christchron <randi_chilz@yahoo.com>
2015-10-12 20:28:34 +0700

    tampilan awal sudah sesuai

--- front/index.php
+++ front/index.php
@@ -2,37 +2,51 @@
 <html>
   <head>
     <title>Stack Exchange</title>
+    <link rel="stylesheet" type="text/css" href="../css/index.css">
   	</head>
   	<body>
-		<h1>Simple StackExchange</h1>
-		<div>
-			<button class="button">Search</button>
-		</div>
-		<div>
-			<p>
-				Cannot find what you are looking for? <a href>Ask here</a>
-			</p>
-		</div>
-		<h2>Recently Asked Question</h2>
-		<?php
-			echo'
-				<div>
-					<h4>0</h4>
-					<h4>Votes</h4>
+  		<div class="container">
+			<h1 class="align-center margin-bot">Simple StackExchange</h1>
+			<div class="row">
+				<div class = "expanse80 expanseleft10">
+					<div class="expanse80">
+						<input type= "text" class ="form">
+					</div>
+					<div class=expanse10>
+						<button class="button">Search</button>
+					</div>
 				</div>
-				<div>
-					<h4>0</h4>
-					<h4>Answer</h4>
-				</div>
-				<div>
-					<p><b>
-						Topic
-					</b></p>	
-					<br>Content</br>
-					<p>
-						asked by email | <a href>edit</a> | <a href>delete</a>
-					</p>
-				</div>';
-		?>
+			</div>
+			<div class="align-center">
+				<p>
+					Cannot find what you are looking for? <a class = "text-link" href><orange>Ask here</orange></a>
+				</p>
+			</div>
+			<h2>Recently Asked Question</h2>
+			<?php
+				echo'
+					<div class="devider">
+						<div class="row">
+							<div class="expanse10">
+								<h4 class="align-center">0</h4>
+								<h4 class="align-center">Votes</h4>
+							</div>
+							<div class="expanse10">
+								<h4 class="align-center">0</h4>
+								<h4 class="align-center">Answers</h4>
+							</div>
+							<div class="expanse80">
+								<p><b>
+									<a class="text-link" href>Topic</a>
+								</b></p>	
+								<br>Content</br>
+								<p class = "align-right footer">
+									asked by <blue>email</blue> | <a class="text-link" href><orange>edit</orange></a> | <a class="text-link" href><red>delete</red></a>
+								</p>
+							</div>
+						</div>
+					</div>
+				';
+			?>
   	</body>
 </html>
