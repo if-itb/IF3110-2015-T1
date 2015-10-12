f1c4e0d53d0db1f6f79c77d52fc72b1e829ec48d
Ryan Yonata <ryan.yonata@gmail.com>
2015-10-12 16:03:31 +0700

    Add delete function from SQL

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+	function RedirectToHome($url, $permanent = false)
+	{
+        header('Location: ' . $url, true, $permanent ? 301 : 302);
+        exit();
+	}
+
+	include('ConnectDatabase.php');
+	$Quest_ID = htmlspecialchars($_GET["id"]);
+	
+	$sql_delete_from_answer = "DELETE FROM Answers WHERE Question_ID = '$Quest_ID'";
+	$result_delete_from_answer = mysqli_query($conn, $sql_delete_from_answer);
+
+	$sql_delete_from_question = "DELETE FROM Questions WHERE ID = '$Quest_ID'";
+	$result_delete_from_question = mysqli_query($conn, $sql_delete_from_question);
+
+	RedirectToHome('home.php', false);
+	$conn->close();
+?>
