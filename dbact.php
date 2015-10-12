5b251a772d58447b910232ad6a76f26e4cc55c58
Ignatius Alriana Haryadi Moel <alrianatcm@gmail.com>
2015-10-13 02:22:38 +0700

    add query for voting

--- dbact.php
+++ dbact.php
@@ -64,6 +64,10 @@
 		$qry = "DELETE FROM question WHERE id_q=$id_q";
 
 		$res = mysqli_query($db, $qry);
+
+		$qry2 = "DELETE FROM answer WHERE id_q=$id_q";
+
+		$res = mysqli_query($db, $qry2);
 		return $res;
 	}
 
@@ -88,5 +92,49 @@
 		$res = mysqli_query($db, $qry);
 
 		return $res;
+	}
+
+	function updateVoteQ($id_q, $act){
+		global $db;
+
+		if($act=="voteUp") $upd=1;
+		else if($act=="voteDown") $upd=-1;
+		$qry = "UPDATE question SET nvote = nvote + '$upd' WHERE id_q = $id_q ";
+
+		$res = mysqli_query($db, $qry);
+
+		return $res;
+	}
+
+	function displayVoteQ($id_q){
+		global $db;
+
+		$qry = "SELECT * FROM question WHERE id_q = $id_q";
+		$res = mysqli_query($db, $qry);
+		$row = mysqli_fetch_array($res, MYSQLI_ASSOC);
+		
+		return $row['nvote'];
+	}
+
+	function updateVoteA($id_q, $id_a, $act){
+		global $db;
+
+		if($act=="voteUp") $upd=1;
+		else if($act=="voteDown") $upd=-1;
+		$qry = "UPDATE answer SET nvote = nvote + '$upd' WHERE id_q = $id_q AND id_a = $id_a";
+
+		$res = mysqli_query($db, $qry);
+
+		return $res;
+	}
+
+	function displayVoteA($id_a){
+		global $db;
+
+		$qry = "SELECT * FROM answer WHERE id_a = $id_a";
+		$res = mysqli_query($db, $qry);
+		$row = mysqli_fetch_array($res, MYSQLI_ASSOC);
+		
+		return $row['nvote'];
 	}
 ?>
