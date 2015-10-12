1b27240b9228c551d7d1a977e412868fe9055a28
hanscg <hcguyz@gmail.com>
2015-10-12 13:33:57 +0700

    Modifying post_question.php
    Removing html tags and adding js

--- mysql/post_question.php
+++ mysql/post_question.php
@@ -1,50 +1,31 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-        
-    </head>
-    <body>
-        <?php
-            $name = $email = $topic = $content = "";
+<?php
+    $name = $email = $topic = $content = "";
             
-            $servername = "localhost";
-            $username = "stckxchg";
-            $password = "";
-            $dbname = "stackExchange";
+    $servername = "localhost";
+    $username = "stckxchg";
+    $password = "";
+    $dbname = "stackExchange";
             
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error . "<br>");
-            }
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error . "<br>");
+    }
             
-            $insert = $conn->prepare("INSERT INTO questions (name, email, topic, content) VALUES (?, ?, ?, ?)");
-            $insert->bind_param("ssss", $name, $email, $topic, $content);
+    $insert = $conn->prepare("INSERT INTO questions (name, email, topic, content) VALUES (?, ?, ?, ?)");
+    $insert->bind_param("ssss", $name, $email, $topic, $content);
                     
-            $name = $_POST['Name'];
-            $email = $_POST['Email']; 
-            $topic = $_POST['Topic']; 
-            $content = $_POST['Content']; 
-            $insert->execute();
-            
-            echo "Question stored<br>";
-            
-            $insert->close();
-            $conn->close();
-
-            /* if ($conn->query($insert) === TRUE) {
-                echo "Question stored<br>";
-            } else {
-                echo "Error storing question: " . $conn->error . "<br>";
-            } */            
-        ?>
-    </body>
-</html>
+    $name = $_POST['Name'];
+    $email = $_POST['Email']; 
+    $topic = $_POST['Topic']; 
+    $content = $_POST['Content']; 
+    $insert->execute();
+    
+    $insert->close();
+    $conn->close();       
+?>
+<script type="text/javascript">
+    alert('Question posted, you will be automatically redirected to home.');
+    window.location = "../index.php";
+</script>
