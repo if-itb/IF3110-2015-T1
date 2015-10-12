7a109c6e09931c0c5f3a8e5cb5c4875b8004b5e5
christchron <randi_chilz@yahoo.com>
2015-10-12 21:19:08 +0700

    added answer.css and answer display fix

--- front/answer.php
+++ front/answer.php
@@ -2,43 +2,60 @@
 <html>
  	<head>
 		<title>Stack Exchange</title>
+		<link rel="stylesheet" type="text/css" href="../css/answer.css">
   	</head>
   	<body>
-		<div>
-			<h1>Simple StackExchange</h1>
+		<div class="container">
+			<h1 class="align-center margin-bot">Simple StackExchange</h1>
 			<?php
 						echo'
 							<h2>Topic</h2>
-							<div>
-								<h2>0</h2>
+							<div class="devider">
+								<div class="row">
+									<div class="expanse10">
+										<div id ="question-arrow-up"></div>
+										<div class="align-center">
+											<h2>0</h2>
+										</div>
+										<div id = "question-arrow-down"></div>
+									</div>
+									<div>
+										<h1>Content</h1>
+									</div>
+									<p class="align-right footer">
+										asked by <blue>email</blue> at 30 Januari | <a href><orange>edit</orange></a> | <a href><red>delete</red></a>
+									</p>
+								</div>
 							</div>
-							<div>
-								<h1>Content</h1>
+							0 Answer
+							<div class="devider">
+								<div class="row">
+									<div class="expanse10">
+										<div class="arrow-up"></div>
+										<div class="align-center">
+											<h2>0</h2>
+										</div>
+										<div class="arrow-down"></div>
+									</div>
+									<div>
+										Content
+									</div>
+									<p class="align-right footer">
+										answered by <blue>email</blue> at 2 Februari
+									</p>
+								</div>
 							</div>
-							<p>
-								asked by email at 30 Januari | <a href>edit</a> | <a href>delete</a>
-							</p>
-							<h2>0 Answer</h2>
-							<div>
-								<h2>0</hw>
-							</div>
-							<div>
-								<h1>Content</h1>
-							</div>
-							<p>
-								answered by email at 2 Februari
-							</p>
 						';
 			?>
 			<hr>
-			<h1>Your Answer</h1>
+			<h1><grey>Your Answer<grey></h1>
 		  	<?php
 				echo'
 					<form name="addAnswer">
-						<input type="text" name="Name"><br>
-						<input type="text" name="Email"> <br>
-						<textarea name="Content"></textarea>
-						<div>
+						<input type="text" class="form" placeholder="Name" name="Name"><br>
+						<input type="text" class="form" placeholder="Email" name="Email"> <br>
+						<textarea class="form" placeholder="Content" rows="5" name="Content"></textarea>
+						<div class="align-right">
 							<button type="submit">Post</button>
 						</div>
 					</form>
