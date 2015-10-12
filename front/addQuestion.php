deef8159ef686d398b4197da35bb52390032d4a7
christchron <randi_chilz@yahoo.com>
2015-10-12 22:11:34 +0700

    added data-question.php, can add new question

--- front/addQuestion.php
+++ front/addQuestion.php
@@ -1,27 +1,26 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title>Stack Exchange</title>
-	    <link rel="stylesheet" type="text/css" href="../css/question.css">
-	  	</head>
-	<body>
+  <head>
+    <title>Stack Exchange</title>
+    <link rel="stylesheet" type="text/css" href="../css/question.css">
+  </head>
+  <body>
 		<div class="container">
-			<h1 class="align-center margin-bot">Simple StackExchange</h1>
-		  	<h2>What's Your Question?</h2>
-		  	<?php		
-				echo'
-				  <hr>
-					<form name="addQuestion">
-						<input type="text" class="form" placeholder="Name" name="Name">
-						<input type="text" class="form" placeholder="Email" name="Email"> 
-				 		<input type="text" class="form" placeholder="Question Topic" name="Topic">
-				  		<textarea class="form" placeholder="Content" rows="5" name="Content"></textarea>
-					  	<div class="align-right">
-							<button type="submit">Post</button>
-					  	</div>
-					</form>
-				';
+		  <h1 class="align-center margin-bot">Simple StackExchange</h1>
+		  <h2>What's Your Question?</h2>
+		  	<?php
+			echo'
+			  <hr>
+				<form name="addQuestion" action="data-question.php" method = "Post">
+				  <input type="text" class="form" placeholder="Name" name="Name">
+				  <input type="text" class="form" placeholder="Email" name="Email"> 
+				  <input type="text" class="form" placeholder="Question Topic" name="Topic">
+				  <textarea class="form" placeholder="Content" rows="5" name="Content"></textarea>
+				  <div class="align-right">
+					<button type="submit">Post</Button>
+				  </div>
+				</form>';
 			?>
-		</div>	
-	</body>
+	</div>	
+  </body>
 </html>
