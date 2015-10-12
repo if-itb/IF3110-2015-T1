ef394e590f426bed8d2bd179f0bdcb7924efd4da
Vincent Theophilus Ciputra <vincent_theophilusc@yahoo.com>
2015-10-12 12:54:35 +0700

    homepage awal dan question page awal dengan css

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Stack Exchange</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <div>
+      <h1>Simple StackExchange</h1>
+      <div>
+        <div>
+          <input>
+        </div>
+        <div>
+          <button>Search</button>
+        </div>
+      </div>
+      <div>
+        <p>
+          Cannot find what you are looking for? <a href="question.php">Ask Here</a>
+        </p>
+      </div>
+	  <h2>Recently Asked Question</h2>
+    </div>
+  </body>
+</html>
---
+++ question.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Stack Exchange</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+    <div>
+      <h1>Simple StackExchange</h1>
+      <h2>What's Your Question?</h2>
+      <hr>
+	<?php
+		echo'
+		  <input type="text" class="form" placeholder="Name">
+		  <input type="text" class="form" placeholder="Email">
+		  <input type="text" class="form" placeholder="Question Topic">
+		  <textarea class="form" placeholder="Content" rows="5"></textarea>
+
+		  <div class="text-right">
+			<button class="btn" type="submit">Post</button>
+		  </div>
+		';
+	?>
+    </div>
+  </body>
+</html>
