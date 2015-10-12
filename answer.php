560dad25ee83766570c38b56e5263ed234cbdbec
candyolivia <candyoliviamawalim@gmail.com>
2015-10-12 11:06:12 +0700

    Voting

--- answer.php
+++ answer.php
@@ -15,8 +15,10 @@
 		$password = "";
 		$dbname = "stackexchange";
 
-		$id = intval($_GET['id']);
+		$vote = -1;
 
+		$id = intval($_GET['id']);
+		$vote;
 		if ( ! empty( $_POST ) ) {
 	 		$user = $_POST['user'];
 	 		$email = $_POST['email'];
@@ -59,21 +61,32 @@
 						<hr>
 						<table>";
 		    	echo "<tr>";
-		    	echo "<td class='arrow'> &#128314 </td>";
-	    		echo '<td class="qacontent" rowspan="3">'.$row["content"].'</td>';
+		    	echo "<td class='arrow' id='vote_up' value= '1'> &#128314 </td>";
+		    	echo '<td class="qacontent" rowspan="3">'.$row["content"].'</td>';
 	    		echo '</tr><tr>';
-	    		echo '<td class="total">'. $row["num_vote"].'</td>';
+	    		echo '<td class="total" id="votes">'.$row["num_vote"];
+	    		echo '</td>';
 	    		echo '</tr><tr>';
-	    		echo '<td class="arrow"> &#128315 </td>';
+	    		echo '<td class="arrow" id="vote_down">&#128315</td>';
 	    		echo '<tr></tr>
 						<tr>
 						<td></td>';
+				$vote = $row['num_vote'];
+		    	
 				echo '<td class="askedby">asked by ';
 	    		echo '<span class="user">'.$row["username"].' </span>at ';
 	    		echo '<span class="time">' .$row["question_date"].' </span>|';
 	    		echo '<span class="edit"><a href="editquestion.php?id='.intval($_GET['id']).'" class="edit"> edit </a></span>|';
 	    		echo '<span class="delete" onclick="ConfirmDelete();"><a href="index.php?id='.intval($_GET['id']).'" class="delete"> delete </a></span>';
 	    		echo "</tr></table><br>";
+
+	    		if ($vote != $row['num_vote']) {
+	    			if(isset($_GET['id'])) {
+				        $add = 'UPDATE question SET num_vote="'.$vote.'" WHERE id_question='. $id;
+						if ($conn->query($add) === TRUE) {
+						}
+					}
+	    		}
 		        
 		    }
 		} else {
@@ -91,22 +104,25 @@
 		    echo "<h2>0 Answer</h2><hr>";
 		}
 		
+	?>
 
+	<?php
 		$sql = "SELECT username, num_votes, content, answer_date, num_answer FROM answer WHERE id_question=" .$id;
 		$result = $conn->query($sql);
 
+		$count = 0;
 		if ($result->num_rows > 0) {
 		    // output data of each row
 		    while($row = $result->fetch_assoc()) {
 		    	
 				echo "<table>";
 		    	echo "<tr>";
-		    	echo "<td class='arrow'> &#128314 </td>";
+		    	echo "<td class='arrow' id='vote_up_ans".$row["num_answer"]."' onclick='voteUpAns(".$row["num_answer"].")'> &#128314 </td>";
 	    		echo '<td class="qacontent" rowspan="3">'.$row["content"].'</td>';
 	    		echo '</tr><tr>';
-	    		echo '<td class="total">'. $row["num_votes"].'</td>';
+	    		echo '<td class="total" id="vote_ans'.$row["num_answer"].'">' . $row["num_votes"].'</td>';
 	    		echo '</tr><tr>';
-	    		echo '<td class="arrow"> &#128315 </td>';
+	    		echo '<td class="arrow" id="vote_down_ans'.$row["num_answer"].'" onclick="voteDownAns('.$row['num_answer'].')"> &#128315 </td>';
 	    		echo '<tr></tr>
 						<tr>
 						<td></td>';
@@ -115,7 +131,7 @@
 	    		echo '<span class="time">' .$row["answer_date"].' </span>';
 	    		echo '</td>';
 	    		echo "</tr></table><br>";   
-
+	    		$temp = $row["num_answer"];
 		    }
 		} else {
 		    echo '<div class="answerresult">No results</div>';
@@ -133,12 +149,84 @@
 		  <input type="submit" value="Post" class="post">
 		</form>'; 
 		$conn->close();
-		
 	?>
-		
-	
-
-	
 </body>
+
+	<script type="text/javascript">
+		document.getElementById('vote_up').onclick = function() 
+		{ 
+			var numvote = parseInt(document.getElementById('votes').innerHTML);
+			var id = <?php Print(intval($_GET['id'])); ?>;
+
+			var xhttp = new XMLHttpRequest()
+		   	numvote++;
+			xhttp.onreadystatechange = function () {
+				if (xhttp.readyState == 4) {
+					// if ok, update
+					document.getElementById("votes").innerHTML = xhttp.responseText;
+					// if not, show error
+				}
+			}
+
+			xhttp.open("GET","numbervote.php?numvote=" + numvote + "&id=" + id, true);
+			xhttp.send();
+		}
+
+		document.getElementById('vote_down').onclick = function() 
+		{ 
+			var numvote = parseInt(document.getElementById('votes').innerHTML);
+			var id = <?php Print(intval($_GET['id'])); ?>;
+
+			var xhttp = new XMLHttpRequest()
+		   	numvote--;
+			xhttp.onreadystatechange = function () {
+				if (xhttp.readyState == 4) {
+					// if ok, update
+					document.getElementById("votes").innerHTML = xhttp.responseText;
+					// if not, show error
+				}
+			}
+
+			xhttp.open("GET","numbervote.php?numvote=" + numvote + "&id=" + id, true);
+			xhttp.send();
+		}
+
+		function voteUpAns(ID_ans) {
+			var numvote = parseInt(document.getElementById('vote_ans'+ID_ans.toString()).innerHTML);
+			var id = <?php Print(intval($_GET['id'])); ?>;
+			var xhttp = new XMLHttpRequest()
+		   	numvote++;
+			xhttp.onreadystatechange = function () {
+				if (xhttp.readyState == 4) {
+					// if ok, update
+					document.getElementById("vote_ans"+ID_ans).innerHTML = xhttp.responseText;
+					// if not, show error
+				}
+			}
+
+			xhttp.open("GET","numbervoteans.php?numvote=" + numvote + "&id=" + id + "&id_ans=" + ID_ans, true);
+			xhttp.send();
+		}
+
+		function voteDownAns(ID_ans) {
+			var numvote = parseInt(document.getElementById('vote_ans'+ID_ans.toString()).innerHTML);
+			var id = <?php Print(intval($_GET['id'])); ?>;
+			var xhttp = new XMLHttpRequest()
+		   	numvote--;
+			xhttp.onreadystatechange = function () {
+				if (xhttp.readyState == 4) {
+					// if ok, update
+					document.getElementById("vote_ans"+ID_ans).innerHTML = xhttp.responseText;
+					// if not, show error
+				}
+			}
+
+			xhttp.open("GET","numbervoteans.php?numvote=" + numvote + "&id=" + id + "&id_ans=" + ID_ans, true);
+			xhttp.send();
+		}
+
+
+
+	</script>
 
 </html>
