100a5a5ef14f8bc8e9ba1540b6573375c77805f3
christchron <randi_chilz@yahoo.com>
2015-10-12 22:18:42 +0700

    added validatequestion.js to validate the input

--- front/addQuestion.php
+++ front/addQuestion.php
@@ -3,6 +3,7 @@
   <head>
     <title>Stack Exchange</title>
     <link rel="stylesheet" type="text/css" href="../css/question.css">
+    <script type = "text/javascript" src="../js/validatequestion.js"> </script>
   </head>
   <body>
 		<div class="container">
@@ -11,7 +12,7 @@
 		  	<?php
 			echo'
 			  <hr>
-				<form name="addQuestion" action="data-question.php" method = "Post">
+				<form name="addQuestion" action="data-question.php" onsubmit="return validateForm()" method = "Post">
 				  <input type="text" class="form" placeholder="Name" name="Name">
 				  <input type="text" class="form" placeholder="Email" name="Email"> 
 				  <input type="text" class="form" placeholder="Question Topic" name="Topic">
