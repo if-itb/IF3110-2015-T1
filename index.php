0eca0ed38f234c9106a7099d4fcb7d3901240d50
raihannurr <raihannurr@gmail.com>
2015-10-13 04:41:33 +0700

    Penambahan tombol edit

--- index.php
+++ index.php
@@ -4,16 +4,60 @@
 	<head>
 		<title>Simple Stack Exchange</title>
 	</head>
-	<body>
-		<H2>SIMPLE STACK EXCHANGE</H2>
+	<body>		
+		<a href="index.php"><H2>SIMPLE STACK EXCHANGE</H2></a>
 		<form action="index.php" method="post">
 			<input type="text" name="keyword">
 			<input type="submit" value="Search">
 			<p id=1>
-				Can't find what you are looking for? <a href="ask.php">Ask Here</a>
+				Can't find what you are looking for? <a href="ask.php?mode=0">Ask Here</a>
 			</p>
-		</form>
+		</form>	
+		<H4>Recently Asked Questions</H4>
+		<?php
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "sse";
+		// Create connection
+		$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+		// Check connection
+		if (!$conn) {
+		    die("Post failed, please resubmit your question.");
+		}
 		
+		//SQL Query
+		$sql ="select * from question";
+		$result = mysqli_query($conn, $sql);
+		
+		
+		if (mysqli_num_rows($result) > 0) {
+		    // output data of each row
+		    while($row = mysqli_fetch_assoc($result)) {
+			$sql = "select * from answer where qid=".$row['qid'];
+			$aresult = mysqli_query($conn, $sql);
+			$ans = mysqli_num_rows($aresult);
+			echo "Votes: ".$row['vote']." -Answers: $ans -Name: " . $row["askname"]. " -Email: " . $row["email"]. 
+			" -Topic: <a href='answer.php?id=".$row['qid']."'>"
+			.$row["topic"].
+			"</a> -Content: ".$row["content"]."
+			<form action='ask.php?mode=1' method='post'>
+				<input type='hidden' name='name' value='".$row["askname"]."'>
+				<input type='hidden' name='qid' value='".$row["qid"]."'>
+				<input type='hidden' name='mail' value='".$row["email"]."'>
+				<input type='hidden' name='topic' value='".$row["topic"]."'>
+				<input type='hidden' name='qcontent' value='".$row["content"]."'>				
+				<button>Edit</button>
+			</form>
+			| Delete <br>";
+		    }
+		} else {
+		    echo "0 results";
+		}
+		
+		mysqli_close($conn);
+		?>
 		
 	
 	</body>
