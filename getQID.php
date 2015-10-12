30918269bbe829700cbe8e31cfd4a37e558d956d
Ryan Yonata <ryan.yonata@gmail.com>
2015-10-12 16:03:51 +0700

    Get ID for hyperlink

---
+++ getQID.php
@@ -0,0 +1,23 @@
+<?php  
+	if(isset($_GET['id']))
+	{
+		include('ConnectDatabase.php');
+		$id = $_GET["id"]; 
+		$sql = "SELECT * FROM Questions WHERE ID = '$_GET[id]' ORDER BY Date_Created DESC";
+		$result = mysqli_query($conn, $sql);
+		$row = mysqli_fetch_assoc($result);
+		$Name	= $row["Name"];
+		$Email 	= $row["Email"];
+		$Topic	= $row["Topic"];
+		$Content= $row["Content"];
+		echo $id;
+	}
+	else
+	{
+		$id= 0; 
+		$Name	= "";
+		$Email 	= "";
+		$Topic	= "";
+		$Content= "";
+	}
+?>
