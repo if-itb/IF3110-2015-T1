c970b822222e088cc8b187d34ff41b9d3f3122ef
Muhamad Fikri Alhawarizmi <mfikria@students.itb.ac.id>
2015-10-12 23:44:55 +0700

    create main layout file
    create main layout file for template web

---
+++ views/layout.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>Simple Stack Exchange</title>
+		<link rel="stylesheet" href="css/style.css"/>
+	</head>
+	
+	<body>
+		<div id="main-wrapper">
+			<header>
+				<div class="logo">
+					<a href="/index.html" class="logo-link">Simple StackExchange</a>
+				</div>
+			</header>
+
+			<main>
+				<?php
+					require_once('router.php');
+				?>
+			</main>
+
+			<footer>
+				<div class="attribution">
+					Created by <a>mfikria</a>
+				</div>
+			</footer>
+		</div>
+	</body>
+</html>
