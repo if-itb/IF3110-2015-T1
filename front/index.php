2a3b31ee4a93ffe53ffa8d1ad76351a14edcbc89
christchron <randi_chilz@yahoo.com>
2015-10-12 21:47:16 +0700

    connect with database, addquestion, answer

--- front/index.php
+++ front/index.php
@@ -12,41 +12,66 @@
 					<div class="expanse80">
 						<input type= "text" class ="form">
 					</div>
-					<div class=expanse10>
+					<div class="expanse10">
 						<button class="button">Search</button>
 					</div>
 				</div>
 			</div>
 			<div class="align-center">
 				<p>
-					Cannot find what you are looking for? <a class = "text-link" href><orange>Ask here</orange></a>
+					Cannot find what you are looking for? <a class ="text-link" href="addQuestion.php"><orange>Ask here</orange></a>
 				</p>
 			</div>
 			<h2>Recently Asked Question</h2>
 			<?php
-				echo'
-					<div class="devider">
-						<div class="row">
-							<div class="expanse10">
-								<h4 class="align-center">0</h4>
-								<h4 class="align-center">Votes</h4>
+				$conn = mysqli_connect("localhost","root","","stackExchange");
+				// Check connection
+				if (mysqli_connect_errno()){
+					echo "Failed to connect to MySQL: " . mysqli_connect_error();
+				}
+				$sql = "SELECT * FROM question";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc()) {
+						echo'
+							<div class="devider">
+								<div class="row">
+									<div class="expanse10">
+										<h4 class="align-center">'.$row["vote"].'</h4>
+										<h4 class="align-center">Votes</h4>
+									</div>
+									<div class="expanse10">
+						';
+						$sql2 = "SELECT * FROM answer where QID = '$row[QID]'";
+						$result2 = $conn->query($sql2);
+						$sumAns = mysqli_num_rows($result2);
+						$subContent = substr($row["Content"],0,50);
+						$cLen = strlen($row["Content"]);
+						$scLen = strlen($subContent);
+						if ($cLen > $scLen ) {
+							$subContent .="....";
+						}
+						echo'
+										<h4 class="align-center">'.$sumAns.'</h4>
+										<h4 class="align-center">Answers</h4>
+									</div>
+									<div class="expanse80">
+										<p><b>
+											<a class ="text-link" href="answer.php?id=' . $row["QID"].'">' . $row["Topic"]. '</a>
+										</b></p>	
+										<br>'.$subContent.'</br>
+										<p class = "align-right footer">
+											asked by <blue>email</blue> | <a class="text-link" href><orange>edit</orange></a> | <a class="text-link" href><red>delete</red></a>
+										</p>
+									</div>
+								</div>
 							</div>
-							<div class="expanse10">
-								<h4 class="align-center">0</h4>
-								<h4 class="align-center">Answers</h4>
-							</div>
-							<div class="expanse80">
-								<p><b>
-									<a class="text-link" href>Topic</a>
-								</b></p>	
-								<br>Content</br>
-								<p class = "align-right footer">
-									asked by <blue>email</blue> | <a class="text-link" href><orange>edit</orange></a> | <a class="text-link" href><red>delete</red></a>
-								</p>
-							</div>
-						</div>
-					</div>
-				';
+						';
+					}
+				} else {
+					echo "0 results";
+				}
 			?>
+		</div>
   	</body>
 </html>
