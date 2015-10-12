5d75177bc01a875fa04377ac81cabc226905c1a1
Ryan Yonata <ryan.yonata@gmail.com>
2015-10-12 17:28:26 +0700

    Add case submission

--- submitQuestions.php
+++ submitQuestions.php
@@ -3,7 +3,7 @@
 	{
         header('Location: ' . $url, true, $permanent ? 301 : 302);
         exit();
-	}	
+	}
 
 	//SubmitQuestions.php
 	include('ConnectDatabase.php');
@@ -14,16 +14,23 @@
 	$Topic = htmlspecialchars($_POST["topic"]);
 	$Content = htmlspecialchars($_POST["content"]);
 	$Today = date("Y-m-d");
+	$id = $_GET['id'];
 
-	$input = mysql_query("INSERT INTO Questions (`Name`, `Email`, `Topic`, `Content`, `Date_Created`) VALUES ('$Name','$Email', '$Topic','$Content', '$Today')");
-
-	/*if ($conn->query($sql) === TRUE) 
+	if ($id == 0)
 	{
-    	echo "New record created successfully";
+		$input = "INSERT INTO Questions (`Name`, `Email`, `Topic`, `Content`, `Date_Created`) VALUES ('$Name','$Email', '$Topic','$Content', '$Today')";
+	}
+	else {
+		$input = "UPDATE Questions SET Name = '$Name', Email = '$Email', Topic = '$Topic', Content = '$Content' WHERE ID = '$id' ";
+	}
+
+	if ($conn->query($input) === TRUE) 
+	{
+		//echo "New record created successfully";
 	} else {
-    	echo "Error: " . $sql . "<br>" . $conn->error;
-	}*/
-	
+		echo "Error: " . $input . "<br>" . $conn->error;
+	}
+	echo $id;
 	RedirectToHome('home.php', false);
 	$conn->close();
 ?>
