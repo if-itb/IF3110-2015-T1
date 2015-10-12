0057c434acea1bb55b64448d4aa4374febf53433
Muhamad Fikri Alhawarizmi <mfikria@students.itb.ac.id>
2015-10-12 23:35:51 +0700

    create connection file
    create connection file for connecting to database using singleton class

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+	class Database {
+			private static $instance = NULL;
+
+			private function __construct(){}
+		
+			private function __clone() {}
+
+			static function getInstance() {
+					$dbname = "db_stackexchange";
+					$host = "localhost";
+					$user = "root";
+					$password = "";
+					if(!isset(self::$instance)){
+							self::$instance = new PDO('mysql:host='.$host.';dbname='.$dbname, $user, $password);
+					}
+					return self:$instance;
+			}
+	}
+?>
