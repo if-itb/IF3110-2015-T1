82e6d5e6f36ee6a84d37c239b17e9066dc6b7a53
raihannurr <raihannurr@gmail.com>
2015-10-13 04:40:04 +0700

    Penambahan fitur edit

--- asked.php
+++ asked.php
@@ -5,8 +5,8 @@
 		<title>ASK a Question</title>
 	</head>
 	<body>
-		<H2>SIMPLE STACK EXCHANGE</H2>	
-		<?php
+		<a href="index.php"><H2>SIMPLE STACK EXCHANGE</H2></a>
+		<?php				
 		$servername = "localhost";
 		$username = "root";
 		$password = "";
@@ -18,12 +18,21 @@
 		if (!$conn) {
 		    die("Post failed, please resubmit your question.");
 		}	
-
+		//SQL Query
 		$val = "'".$_POST['name']."','".$_POST['mail']."','".$_POST['topic']."','".$_POST['qcontent']."'";
 		
-		$sql = "insert into question (askname, email, topic, content)
-		values (".$val.")";
-		
+		if ($_POST['mode']==0) {
+			$sql = "insert into question (askname, email, topic, content)
+			values (".$val.")";
+		}
+		else {
+			$sql = "update question	set 
+			askname='".$_POST['name']
+			."',email='".$_POST['mail']
+			."',topic='".$_POST['topic']
+			."',content='".$_POST['qcontent']
+			."' where (qid=".$_POST['qid'].")";
+		}
 		if (mysqli_query($conn, $sql)) {
 		    echo "Your question has been posted succesfully!";
 		} else {
