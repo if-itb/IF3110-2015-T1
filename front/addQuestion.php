5995881df1c0ae2616ef398dff550c47a079423b
christchron <randi_chilz@yahoo.com>
2015-10-12 20:35:09 +0700

    tampilan index(fix), addquestion(on going)

---
+++ front/addQuestion.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Stack Exchange</title>
+  </head>
+  <body>
+		  <h1>Simple StackExchange</h1>
+		  <h2>What's Your Question?</h2>
+		  	<?php		
+				echo'
+				  <hr>
+					<form>
+					  <input type="text" ><br>
+					  <input type="text" > <br>
+					  <input type="text" ><br>
+					  <textarea></textarea>
+					  <div>
+						<button type="submit">Post</button>
+					  </div>
+					</form>
+				';
+			?>
+	</div>	
+  </body>
+</html>
