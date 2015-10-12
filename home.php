83f1a5f815e3f42ba926b68924b2be11dce7500d
dEUS00 <deus.caj@gmail.com>
2015-10-12 14:34:04 +0700

    Styling dengan css

--- home.php
+++ home.php
@@ -2,17 +2,19 @@
 <html>
 <head>
 	<title>
-		Simple Stack Exchange
+		Simple StackExchange
 	</title>
+	<link rel='stylesheet' type="text/css" href="css/sst.css">
 </head>
 
 <body>
-	<h1 align="center">Simple Stack Exchange</h1>
+	<h1 align="center">Simple StackExchange</h1>
 	<br>
 	<br>
-	<h3 align="center">Cannot find what you are looking for? <a href="create.php">Ask here</a></h3>
-	<h3 align="center">Recently Asked Questions</h3>
-	<hr width="75%" align="center">
+	<h3 id="ask_here">Cannot find what you are looking for? <a href="create.php">Ask here</a></h3>
+	<br>
+	<h3 id="recent">Recently Asked Questions</h3>
+	<hr>
 	<?php
 		$conn = mysql_connect('localhost', 'root', '08161955342');
 		mysql_select_db('simplestackexchange', $conn);
@@ -50,14 +52,32 @@
 		$sql = "SELECT * FROM question";
 		$result = mysql_query($sql);
 	?>
-	<div align="center">
+	<div class="question">
 	<?php while ($row = mysql_fetch_assoc($result)) { ?>
-		<?php echo $row['vote'] . " "; ?>
-		<a href="answer.php?question_id=<?php echo $row['id']; ?>"><?php echo $row['topic'] . " "; ?></a>
-		<?php echo "asked by ". $row['name'] . " "; ?>
-		| <a href="create.php?question_id=<?php echo $row['id']; ?>">edit</a> | delete
-		<br>
-		<hr width="75%" align="center">
+		<div class="question" id="votes">
+			<?php echo $row['vote'] . " "; ?> <div>Vote(s)</div>
+		</div>
+		<div class="question" id="answers">
+			<?php
+				$answer_count_sql = "SELECT count(id) as c FROM answer WHERE question_id='".$row['id']."'";
+				$answer_count = mysql_fetch_assoc(mysql_query($answer_count_sql));
+				echo $answer_count['c'];
+			?> <div>Answer(s)</div>
+		</div>
+		<div class="question" id="topic">
+			<a href="answer.php?question_id=<?php echo $row['id']; ?>"><?php echo $row['topic'] . " "; ?></a>
+		</div>
+		<div class="question" id="asked_by">
+			<br>
+			<br>
+			<?php echo "asked by "?>
+			<font color="#0024ff">
+				<?php echo $row['name'] . " "; ?>
+			</font>
+			| <a href="create.php?question_id=<?php echo $row['id']; ?>"><font color="ffdb00">edit</font></a> | 
+			<font color="#ff1a00">delete</font>
+		</div>
+		<hr>
 	<?php } ?>
 	</div>
 	<?php mysql_close($conn); ?>
