7453a85f0665bf429ff8acc66493ed22079be9bd
raihannurr <raihannurr@gmail.com>
2015-10-13 01:31:56 +0700

    create search form

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title>Simple Stack Exchange</title>
+	</head>
+	<body>
+		<form action="index.php" method="post">
+			<input type="text" name="keyword">
+			<input type="submit" value="Search">
+			<p id=1>
+				Can't find what you are looking for? <a href="index.php">Ask Here</a>
+			</p>
+		</form>
+		
+		
+	
+	</body>
+</html>
 
