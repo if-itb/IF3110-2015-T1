5c34c050578a55cd0fea446ab9ce112133938f53
Luminto 13513080 <luminto_luhur@hotmail.com>
2015-10-12 15:37:19 +0700

    update Voting and fixing bug

--- wbd.php
+++ wbd.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html>
+<title>Homepage</title>
 <head>
 <link rel="stylesheet" type="text/css" href="wbd.css">
 </head>
 <body>
 <h1>Simple Stack Exchange</h1>
-<form name="SearchQuestion">
+ <form name="SearchQuestion">
 	<input id="SearchBox" type="text" placeholder="Search..." required">
 	<input id="SubmitButton "type="button" value="Search"><br>
 </form>
@@ -24,10 +25,32 @@
 	//echo "Success: A proper connection to MySQL was made! The WBD database is great" . PHP_EOL;
 	//echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 	if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-		$sql = "INSERT INTO question(Nama, Email, QuestionTopic, Content, Vote) VALUES ('$_POST[name]','$_POST[email]','$_POST[questiontopic]','$_POST[content]',0)";
-		if(!mysqli_query($link,$sql)){
-			echo "Input data failed";
+		if(isset($_GET["id"]) && isset($_GET["rule"])){
+			if ($_GET["rule"] == "edit"){
+				$sql = "UPDATE question SET Nama = '$_POST[name]', Email = '$_POST[email]', QuestionTopic = '$_POST[questiontopic]', content = '$_POST[content]' WHERE IDQ = '$_GET[id]'";
+				if(mysqli_query($link,$sql)){
+					
+				}
+			}
+		} else {
+			$sql = "INSERT INTO question(Nama, Email, QuestionTopic, Content, Vote) VALUES ('$_POST[name]','$_POST[email]','$_POST[questiontopic]','$_POST[content]',0)";
+			if(!mysqli_query($link,$sql)){
+			
+			}
 		}
+	}
+	
+	if (isset($_GET["rule"])){
+		if ($_GET["rule"] == "delete") {
+			$sql = "DELETE FROM question WHERE IDQ = '$_GET[id]'";
+			if(mysqli_query($link,$sql)){
+				
+			}
+			$sql = "DELETE FROM answer WHERE IDQ = '$_GET[id]'";
+			if(mysqli_query($link,$sql)){
+				
+			}
+		}	
 	}
 
 	$sql = "SELECT * FROM QUESTION";
@@ -36,8 +59,8 @@
 		throw new My_Db_Exception ('Database error: ' . mysql_error());
 	}
 	
-	
-	while ($row = $retval->fetch_assoc()){
+	if ($retval->num_rows > 0){
+		while ($row = $retval->fetch_assoc()){
 		$sql4 = "SELECT COUNT(answer.IDAns) AS JumlahJawaban FROM answer WHERE answer.IDQ=" . $row['IDQ'];
 		$retval4 = mysqli_query($link,$sql4);
 		if(!$retval4){
@@ -52,12 +75,17 @@
 			$temp = $row2['JumlahJawaban'];
 		}
 		echo "<td class = 'td1'> " .$temp . "<br>Answers</td>";
-		echo "<td id = 'td2'> <a href = 'answer.php?id=" . $row['IDQ'] . "'>" . $row['QuestionTopic'] . "</a></td>";
-		echo "<td id = 'td3'> <p id='huruf2'>asked by <style='color:blue'>" . $row['Nama'] . " | edit | delete </p></td>";
+		echo "<td id = 'td2'> <a href = 'answer.php?id=" . $row['IDQ'] . "' style = 'color:black'>" . $row['QuestionTopic'] . "</a><br><br>" .$row['Content'] . "</td>";
+		echo "<td id = 'td3'> <p id='huruf2'>asked by 
+		<span style='color:blue'>" . $row['Nama'] . "</span> | 
+		<a href = 'wbd2.php?id=" .$row ['IDQ']. "' style = 'color:yellow'> edit </a> | 
+		<a href = 'wbd.php?id=" .$row ['IDQ'] . "&rule=delete' style = 'color:red'> delete </a></p></td>";
 		echo "</tr>";
 		echo "</table>";
 		echo "</div>";
+	}	
 	}
+	
 	mysqli_close($link);
 	?>
 </div>
