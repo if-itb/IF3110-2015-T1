5b4f2678191b7643d1c7c9767946bf76530af6c3
Aufar Gilbran <aufargilbran@gmail.com>
2015-10-12 15:10:37 +0700

    Added fonts for title and text, trying to make search box border work

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<link href='https://fonts.googleapis.com/css?family=Poiret+One|Roboto' rel='stylesheet' type='text/css'>
 		<?= $metas ?>
 		<?= $styles ?>
 		<?= $scripts ?>
--- resources/views/questions/search.php
+++ resources/views/questions/search.php
@@ -1,6 +1,9 @@
 <div class='search'>
-	<form method='GET' action='/public/search'>
-		<input class='search-bar' type='textbox' name='params' placeholder='Search questions here...'>
+	<form class='search-form' method='GET' action='/public/search'>
+		<input class='search-bar' type='search' name='params' placeholder='Search Lyz here...'>
 		<button class='search-button' type='submit'>Submit</button>
 	</form>
+	<div class='search-not-found'>
+		<span>Cannot find what you're looking for? <a href='/ask'>Create new Lyz here.</a></span>
+	</div>
 </div>
