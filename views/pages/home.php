b23d505ff939a48d89bf3c7351f4034aa102b3a4
Muhamad Fikri Alhawarizmi <mfikria@students.itb.ac.id>
2015-10-13 01:16:03 +0700

    update view home

---
+++ views/pages/home.php
@@ -0,0 +1,19 @@
+<form class="form-wrapper">
+	<input type="text" placeholder="Search here..." required>
+	<button type="submit">Search</button>
+	<div class="clearfix"></div>
+	<div id="suggestion">
+			Cannot find what you are looking for? <a href="askform.php">ask here</a>.
+	</div>
+</form>
+
+<h1>
+	Recently Asked Questions
+</h1>
+				
+<div class="question-list">
+
+<?php
+	require_once('views/questions/index.php');
+?>
+</div> <!-- END question-list -->
