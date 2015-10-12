c98f4ec51b28e93d3dad43897937acebadd76aeb
Luminto 13513080 <luminto_luhur@hotmail.com>
2015-10-12 15:35:58 +0700

    Voting down for Question

---
+++ votedownQ.php
@@ -0,0 +1,24 @@
+<?php
+	$link = mysqli_connect("127.0.0.1","root","","wbd");
+	if (!$link){
+		echo "Error: Unable to connect to MySQL." . PHP_EOL;
+		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+		exit;
+	}
+	if (isset($_GET["id"])){
+		$sql = "UPDATE question SET Vote = Vote - 1 WHERE IDQ = '$_GET[id]'";
+		if(mysqli_query($link,$sql)){
+			
+		}
+	}
+	$sql = "SELECT Vote From Question WHERE IDQ = '$_GET[id]'";
+	$retval = mysqli_query($link,$sql);
+	if(!$retval){
+		throw new My_Db_Exception ('Database error: ' . mysql_error());
+	}
+	if ($row = $retval->fetch_assoc()){	
+		echo $row['Vote'];
+	}
+	mysqli_close($link);
+?>
