0d77faa9ad5ba85386bfa094e3840aa9444492f9
christchron <randi_chilz@yahoo.com>
2015-10-12 20:42:30 +0700

    display index(fix), addquestion(fix)

--- front/addQuestion.php
+++ front/addQuestion.php
@@ -1,25 +1,27 @@
 <!DOCTYPE HTML>
 <html>
-  <head>
-    <title>Stack Exchange</title>
-  </head>
-  <body>
-		  <h1>Simple StackExchange</h1>
-		  <h2>What's Your Question?</h2>
+	<head>
+		<title>Stack Exchange</title>
+	    <link rel="stylesheet" type="text/css" href="../css/question.css">
+	  	</head>
+	<body>
+		<div class="container">
+			<h1 class="align-center margin-bot">Simple StackExchange</h1>
+		  	<h2>What's Your Question?</h2>
 		  	<?php		
 				echo'
 				  <hr>
-					<form>
-					  <input type="text" ><br>
-					  <input type="text" > <br>
-					  <input type="text" ><br>
-					  <textarea></textarea>
-					  <div>
-						<button type="submit">Post</button>
-					  </div>
+					<form name="addQuestion">
+						<input type="text" class="form" placeholder="Name" name="Name">
+						<input type="text" class="form" placeholder="Email" name="Email"> 
+				 		<input type="text" class="form" placeholder="Question Topic" name="Topic">
+				  		<textarea class="form" placeholder="Content" rows="5" name="Content"></textarea>
+					  	<div class="align-right">
+							<button type="submit">Post</button>
+					  	</div>
 					</form>
 				';
 			?>
-	</div>	
-  </body>
+		</div>	
+	</body>
 </html>
