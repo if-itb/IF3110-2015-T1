2e7d95b3444ef5ef486b9f02f658b7e5bd5d0a8b
christchron <randi_chilz@yahoo.com>
2015-10-12 20:02:09 +0700

    Index.php
    Tampilan sederhana

---
+++ front/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <title>Stack Exchange</title>
+  	</head>
+  	<body>
+		<h1>Simple StackExchange</h1>
+		<div>
+			<button class="button">Search</button>
+		</div>
+		<div>
+			<p>
+				Cannot find what you are looking for? <a href>Ask here</a>
+			</p>
+		</div>
+		<h2>Recently Asked Question</h2>
+		<?php
+			echo'
+				<div>
+					<h4>0</h4>
+					<h4>Votes</h4>
+				</div>
+				<div>
+					<h4>0</h4>
+					<h4>Answer</h4>
+				</div>
+				<div>
+					<p><b>
+						Topic
+					</b></p>	
+					<br>Content</br>
+					<p>
+						asked by email | <a href>edit</a> | <a href>delete</a>
+					</p>
+				</div>';
+		?>
+  	</body>
+</html>
