df13d7c2159e03e271cec30f3012804396b6770f
raihannurr <raihannurr@gmail.com>
2015-10-13 02:53:54 +0700

    create page for question asking

---
+++ ask.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title>ASK a Question</title>
+	</head>
+	<body>
+		<H2>SIMPLE STACK EXCHANGE</H2>		
+		<form action="asked.php" method="post">
+		<input type="text" name="name"><br>
+		<input type="text" name="mail"><br>
+		<input type="text" name="topic"><br>
+		<textarea name="qcontent" cols=50 rows=5></textarea><br>
+		<input type="submit" value="Post">
+		</form> 
+
+	</body>
+</html>
