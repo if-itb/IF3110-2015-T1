3e40a88f9120dc73bb5666a562473f15e7f36227
fitrakun <fitrakun@gmail.com>
2015-10-13 11:47:50 +0700

    list with css

---
+++ list.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Web for Questions and Answers">
+        <meta name="author" content="13513095 Fitra Rahmamuliani">
+        <link href='https://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Dosis:500' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Simple Stack Exchange</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="title">Simple StackExchange</div>
+            <div class="containersearch">
+                <form class="formwrap formsearch">
+                    <input type="text" id="search" placeholder="Search Questions..." />
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+            <div class="textunder">
+                <p class="text1">Cannot find what you are looking for?</p><a class="ah"href="#">Ask here</a>
+            </div>
+            <p class="raqtext">Recently Asked Questions</p>
+            <div class="raq">
+                <div class="votepart">
+                    <div class="votenumber">0</div>
+                    <div class="votetext">Votes</div>
+                </div>
+                <div class="answerpart">
+                    <div class="answernumber">0</div>
+                    <div class="answertext">Answers</div>
+                </div>
+                <div class="questionpart">
+                    <div class="qtopic">
+                        The question topic goes here
+                    </div>
+                    <div class="qcontent">
+                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+                <div class="labelunder">
+                    <p class="ab">asked by </p>
+                    <a href="#" class="name">name </a>
+                    <a href="#" class="edit">edit </a>
+                    <a href="#" class="delete">delete </a>
+                </div>
+            </div>
+            <div class="raq">
+                <div class="votepart">
+                    <div class="votenumber">0</div>
+                    <div class="votetext">Votes</div>
+                </div>
+                <div class="answerpart">
+                    <div class="answernumber">0</div>
+                    <div class="answertext">Answers</div>
+                </div>
+                <div class="questionpart">
+                    <div class="qtopic">
+                        The question topic goes here
+                    </div>
+                    <div class="qcontent">
+                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+                <div class="labelunder">
+                    <p class="ab">asked by </p>
+                    <a href="#" class="name">name </a>
+                    <a href="#" class="edit">edit </a>
+                    <a href="#" class="delete">delete </a>
+                </div>
+            </div>
+            <div class="raq">
+                <div class="votepart">
+                    <div class="votenumber">0</div>
+                    <div class="votetext">Votes</div>
+                </div>
+                <div class="answerpart">
+                    <div class="answernumber">0</div>
+                    <div class="answertext">Answers</div>
+                </div>
+                <div class="questionpart">
+                    <div class="qtopic">
+                        The question topic goes here
+                    </div>
+                    <div class="qcontent">
+                       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+                <div class="labelunder">
+                    <p class="ab">asked by </p>
+                    <a href="#" class="name">name </a>
+                    <a href="#" class="edit">edit </a>
+                    <a href="#" class="delete">delete </a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
