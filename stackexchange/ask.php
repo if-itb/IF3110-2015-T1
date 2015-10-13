16ee142e67c069c0d25e05453735b58a7751bfcb
jessicahjkl <luvmilk_xoxo@yahoo.com>
2015-10-13 15:40:24 +0700

    Updated ask and index with PHP
    ask.php and index.php

--- stackexchange/ask.php
+++ stackexchange/ask.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -6,7 +8,13 @@
 <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 
+
+
 <body>
+<?php
+	$connect = mysql_connect("localhost","root","") or die ("Connection Error");
+	$selectdb = mysql_select_db("stackexchange", $connect);
+?>
 <h1> Simple Stack Exchange </h1>
 <div id="container">
 <b> What's your question? </b>
@@ -16,9 +24,27 @@
 	<p></p><input type="text" name="email" placeholder="Email" required>
     <p></p><input type="text" name="topic" placeholder="Question Topic" required />
     <p></p><textarea name="content" placeholder="Content" required="required"></textarea>
-	<p></p><button type="submit"> Post </button>
+	<p></p><button type="submit" name="submit"> Post </button>
 </form>
 
 </div>
 </body>
+
+<?php
+if (array_key_exists('submit',$_POST)){
+$name = $_POST['name'];
+$email = $_POST['email'];
+$topic = $_POST['topic'];
+$question = $_POST['content'];
+
+
+mysql_query ("INSERT INTO `question`(`name`, `email`, `topic`, `question_content`, `votes`) VALUES ('$name','$email','$topic','$question','0')",$connect);
+header("Location: index.php");
+}
+mysql_close($connect);
+
+?>
+
 </html>
+
+
--- stackexchange/index.php
+++ stackexchange/index.php
@@ -2,42 +2,52 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
 <title>Simple Stack Exchange</title>
 <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
+
+<?php
+	$connect = mysql_connect("localhost","root","") or die ("Connection Error");
+	$selectdb = mysql_select_db("stackexchange", $connect);
+?>
+
 <body>
 	<h1> Simple Stack Exchange </h1>    
 <form class="search" action="result.php" method="get">
-	<input type="text" name="q" placeholder="Looking for something?" required class="search-box">
-	<button type="submit"> Search </button>
-</form>
-Cannot find what you are looking for? <a href="ask.php">Ask here </a>
+		<input type="text" name="q" placeholder="Looking for something?" required class="search-box">
+		<button type="submit"> Search </button>
+	</form>
+	Cannot find what you are looking for? <a href="ask.php">Ask here </a>
 
-<div id="container">
-<b>Recently asked questions</b>
-<?php
-echo '<table class=questions-table>';
-    	echo '<tr>';
-        echo '<td rowspan="2" class="td-vote-answer">';
-			echo '<b>0</b><br>votes';
-		echo '</td>';
-		    echo '<td rowspan="2" class="td-icon">';
-			echo '<b>0</b><br>answers';
-		echo '</td>';
-        echo '<td class="td-topic">';
-			echo 'The question topic goes here';
-			
-		echo '</td>';
-        echo '</tr>';
-        echo '<tr>';
-        echo '<td class="td-detail">';
-			echo 'Asked by ........';
-			//echo $result[$i]->directory;
-		echo '</td>';
-        echo '</tr>';
-    echo '</table>';
-?>
+	<div id="container">
+		<b>Recently asked questions</b>
+        
+        <?php
+		$data = mysql_query("SELECT * FROM `question` ORDER BY `timestamp` DESC limit 5" ,$connect);
+		
+		while($row = mysql_fetch_array($data, MYSQL_NUM)){
+        echo '<table class=questions-table>';
+                echo '<tr>';
+                echo '<td rowspan="2" class="td-vote-answer">';
+                    echo ($row[6].'<br>votes');
+                echo '</td>';
+                    echo '<td rowspan="2" class="td-icon">';
+                    echo '<b>0</b><br>answers';
+                echo '</td>';
+                echo '<td class="td-topic">';
+                    echo $row[4];
+                    
+                echo '</td>';
+                echo '</tr>';
+                echo '<tr>';
+                echo '<td class="td-detail">';
+                    echo ('Asked by '.$row[2]);
+                    //echo $result[$i]->directory;
+                echo '</td>';
+                echo '</tr>';
+            echo '</table>';
+		}
+        ?>
 <table class="questions-table">
 <tr>
 <td></td>
