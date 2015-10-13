f40ff04429b671318541abbe0bee52f1ec943e38
christchron <randi_chilz@yahoo.com>
2015-10-13 21:31:12 +0700

    update answer.php(validate before delete)

--- front/answer.php
+++ front/answer.php
@@ -4,6 +4,7 @@
 		<title>Stack Exchange</title>
 		<link rel="stylesheet" type="text/css" href="../css/answer.css">
 		<script type = "text/javascript" src="../js/validateanswer.js"> </script>
+		<script type = "text/javascript" src="../js/validatedelete.js"> </script>
   	</head>
   	<body>
 		<div class="container">
@@ -39,7 +40,7 @@
 										'.$row["Content"].'
 									</div>
 									<p class="align-right footer">
-										asked by <blue>'.$row["Email"].'</blue> at '.$row["Date"].' | <a class ="text-link" href="updateQuestion.php?id='.$row["QID"].'"><orange>edit</orange></a> | <a class ="text-link" href ="delete.php?id='.$row["QID"].'"><red>delete</red></a>
+										asked by <blue>'.$row["Email"].'</blue> at '.$row["Date"].' | <a class ="text-link" href="updateQuestion.php?id='.$row["QID"].'"><orange>edit</orange></a> | <a class ="text-link" href ="delete.php?id='.$row["QID"].'" onclick="return validateDelete()"><red>delete</red></a>
 									</p>
 								</div>
 							</div>
