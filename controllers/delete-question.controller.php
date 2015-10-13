edb39098e86eecfda2810e6300edad8ebbd38899
tifaniwarnita <tiffayumuyuka@gmail.com>
2015-10-13 17:25:35 +0700

    Fix query for deleting question
    Need to consider the foreign key

--- controllers/delete-question.controller.php
+++ controllers/delete-question.controller.php
@@ -7,6 +7,15 @@
 		global $conn;
 
 		$q_id = mysqli_real_escape_string($conn, $id);
+		
+		$sql = "DELETE FROM answer WHERE q_id='$q_id'";
+
+		if (mysqli_query($conn, $sql)) {
+			header('location: ../');
+		} else {
+			echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+		}
+
 		// Delete the question in database
 		$sql = "DELETE FROM question WHERE id='$q_id'";
 
---
+++ controllers/home.controller.php
@@ -0,0 +1,31 @@
+<?php
+	require_once("config/config.php");
+
+	// Retrieve all questions that have been asked
+	function retrieveQuestions() {
+		global $conn;
+
+		$sql = "SELECT * FROM question;";
+		$result = mysqli_query($conn, $sql);
+
+		$arr = array();
+
+		if (mysqli_num_rows($result) > 0) {
+			// Assign every data of each row to array of question
+			while ($row = mysqli_fetch_assoc($result)) {
+				$question = array();
+				$question["id"] = $row["id"];
+				$question["name"] = $row["name"];
+				$question["email"] = $row["email"];
+				$question["topic"] = $row["topic"];
+				$question["content"] = $row["content"];
+				$question["vote"] = $row["vote"];
+				$question["answer"] = $row["answer"];
+				$question["date_created"] = $row["date_created"];
+				$question["date_edited"] = $row["date_edited"];
+				array_push($arr, $question);
+			}
+		}
+		return $arr;
+	}
+?>
