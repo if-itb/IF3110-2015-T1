55f1ab4bed41674fb62a8845e594dfcd397474db
deredet <lucky.cahyadi@yahoo.com>
2015-10-13 23:08:33 +0700

    adding more function

--- aDBFunct.php
+++ aDBFunct.php
@@ -36,4 +36,54 @@
 			return 0;
 		}	
 	}
+	function countAnswer($qid){
+		global $servername, $username,$password,$dbname;
+		//Create connection
+		$conn = mysqli_connect($servername,$username,$password,$dbname);
+		//Check Connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
+		$sql = "SELECT Count(*) as count FROM Answers where qid=$qid";
+		$result = mysqli_query($conn,$sql);
+		return $result;
+		mysqli_close($conn);
+	}
+	function voteUpAnswer($aid,$up){
+		global $servername, $username,$password,$dbname;
+		
+		$conn = mysqli_connect($servername, $username, $password, $dbname);
+		// Check connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
+		if($up==1){
+			$plus = 1;
+		}else {
+			$plus =-1;
+		}
+		$sql = "UPDATE Answers SET aVote=(aVote+$plus)  WHERE aid=$aid";
+
+		if (mysqli_query($conn, $sql)) {
+			return 1;
+		} else {
+			echo "Error updating record: " . mysqli_error($conn);
+			return 0;
+		}
+
+		mysqli_close($conn);
+	}
+	function getAVote($aid){
+		global $servername, $username,$password,$dbname;
+		//Create connection
+		$conn = mysqli_connect($servername,$username,$password,$dbname);
+		//Check Connection
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
+		$sql = "SELECT aVote FROM Answers WHERE aid=$aid;";
+		$result = mysqli_query($conn,$sql);
+		return $result;
+		mysqli_close($conn);
+	}
 ?>
