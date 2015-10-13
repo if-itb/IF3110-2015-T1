8fc8df9a2efe5c59a4c3398ea9ceab2ee63e5ee4
jsavigny <jsavigny@gmail.com>
2015-10-13 21:15:25 +0700

    some style change, remove db password

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 						$servername = "localhost";
 						$username = "root";
-						$password = "123456";
+						$password = "";
 						$dbname = "simplestackexchange";
 						$conn = new mysqli($servername, $username, $password,$dbname);
 						if ($conn->connect_error) {
