826ce8f9bc439619f53a31c3475108f0d7d3341c
Ahmad Darmawan <adarwawan@gmail.com>
2015-10-14 20:29:56 +0700

    Remake Front-End
    Mendesain ulang index.php dan question.php beserta stylenya

--- index.php
+++ index.php
@@ -1,70 +1,26 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
-	<title>Home - Simple Stack Exchange</title>
+	<title>Home - Simple StackExchange</title>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-	<div class="container">
-		<div class="header">
-			<h1 id="title">Simple Stack Exchange</h1>
-		</div>
-	
-		<div class="wrapper">
-			<div class="search-tab">
-				<form>
-					<input type="text" name="search" id="search-bar" placeholder="What you are looking for?">
-					<input type="submit" value="Search" id="search-button">
-				</form>
-				<p>Cannot find what you are looking for? <a href="#" id="ask-here">Ask Here</a></p>
-			</div>
-
-			<div class="recent">
-				<h3 id="recent-title">Recently Asked Questions</h3>
-
-				<div class="question">
-					<div id="votes">
-						0
-						<div>Votes</div>
-					</div>
-					<div id="answers">
-						0
-						<div>Answers</div>
-					</div>
-					<div id="question-title">
-						The question topic goes here
-					</div>
-					<div id="question-contain">
-						Bla bla bla
-					</div>
-					<div id="question-info">
-						asked by name | edit | delete
-					</div>
-				</div>
-
-				<div class="question">
-					<div id="votes">
-						0
-						<div>Votes</div>
-					</div>
-					<div id="answers">
-						0
-						<div>Answers</div>
-					</div>
-					<div id="question-title">
-						The question topic goes here
-					</div>
-					<div id="question-contain">
-						Bla bla bla
-					</div>
-					<div id="question-info">
-						asked by name | edit | delete
-					</div>
-				</div>
-			</div>
-		</div>
+	<div class="header-page">
+		<h1>Simple StackExchange</h1>
 	</div>
+
+	<div class="search-form">
+		<form>
+			<input id="search-bar" name="search-bar" type="text" placeholder="What are you looking for?">
+			<input id="search-button" name="search-button" type="submit" value="Search">
+		</form>
+	</div>
+	<p>Cannot find what you are looking for? <a href="#" id="ask-here">Ask Here </a></p>
+	<h3>Recently Asked Question</h3>
+	<hr>
 </body>
 </html>
--- question.php
+++ question.php
@@ -1,30 +1,27 @@
 <!DOCTYPE html>
+
 <html>
 <head>
-	<title>Home - Simple Stack Exchange</title>
+	<title>Question - Simple StackExchange</title>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
-	<div class="container">
-		<div class="header">
-			<h1 id="title">Simple Stack Exchange</h1>
-		</div>
+	<div class="header-page">
+		<h1>Simple StackExchange</h1>
+	</div>
 
-		<div class="question">
-			<h3 id="wh-question">What's your question?</h3>
-			<hr>
-
-			<form name="question-form" class="question-form">
-				<div><input type="text" id="name" placeholder="Name"></div>
-				<div><input type="text" id="email" placeholder="Email"></div>
-				<div><input type="text" id="topic" placeholder="Question Topic"></div>
-				<div><textarea rows="10" id="content" cols="106" placeholder="Content"></textarea></div>
-
-				<input type="submit" id="post-button" value="Post">
-			</form>
-		</div>
+	<h3>What's your question?</h3>
+	<hr>
+	<div class="question-form">
+		<form>
+			<div><input type="text" id="name" name="name" placeholder="Name"></div>
+			<div><input type="text" id="email" name="email" placeholder="Email"></div>
+			<div><input type="text" id="topic" name="topic" placeholder="Topic"></div>
+			<div><textarea id="content" name="content" placeholder="Content" rows="5" cols="40"></textarea></div>
+			<div><input id="postbutton" type="submit" value="Post"></div>
+		</form>
 	</div>
 </body>
-
 </html>
