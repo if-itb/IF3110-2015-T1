f757d3ccad148322eed2dddad56a94eb4b6f9047
hanscg <hcguyz@gmail.com>
2015-10-14 18:28:35 +0700

    Displaying question in edit form

--- edit.php
+++ edit.php
@@ -1,3 +1,15 @@
+<?php
+	require_once("./mysql/retrieve_qa.php");
+        $questions = retrieveOneQuestion($_GET['id']);
+        $answers = retrieveAnswers($_GET['id']);
+        
+        foreach ($questions as $question) :
+            $name = $question['name'];
+            $email = $question['email'];
+            $topic = $question['topic'];
+            $content = $question['content'];
+        endforeach;
+?>
 <html>
     <head>
         <meta charset="UTF-8">
@@ -12,11 +24,12 @@
             <h2>What's your question?</h2>
         </div>
         <div>
+            
             <form name="edit_q" action="./mysql/edit_question.php" onsubmit="return validateQuestionForm('edit_q')" method="post">
-                <input type="text" name="Name"><br>
-                <input type="text" name="Email"><br>
-                <input type="text" name="Topic"><br>
-                <input type="text" name="Content"><br>
+                <input type="text" name="Name" value="<?php echo $name ?>"><br>
+                <input type="text" name="Email" value="<?php echo $email ?>"><br>
+                <input type="text" name="Topic" value="<?php echo $topic ?>"><br>
+                <input type="text" name="Content" value="<?php echo $content ?>"><br>
                 <input type="hidden" name="Question_ID" value="<?php echo $_GET['id'] ?>"><br>
                 <input type="submit">
             </form>
