3fc4feab6e1b46c0e5c834180bd797eb3c186746
raihannurr <raihannurr@gmail.com>
2015-10-14 18:28:12 +0700

    Penambahan validasi

--- ask.php
+++ ask.php
@@ -6,12 +6,13 @@
 	</head>
 	<body>		
 		<a href="index.php"><H2>SIMPLE STACK EXCHANGE</H2></a>	
-		<form action="asked.php" method="post">
-		<input type="text" name="name"><br>
-		<input type="text" name="mail"><br>
-		<input type="text" name="topic"><br>
+		<H3>WHAT'S YOUR QUESTION?</H3>
+		<form name="qForm"  method="post" onsubmit="validateForm()">
+		<textarea id='name' name="name" onfocus="if (this.value == 'Name') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Name';}">Name</textarea><br>
+		<textarea id='mail' name="mail" onfocus="if (this.value == 'Email') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Email';}">Email</textarea><br>
+		<textarea id='topic' name="topic" onfocus="if (this.value == 'Question Topic') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Question Topic';}">Question Topic</textarea><br>
 		<input type="hidden" name="mode" value= <?php echo $_GET['mode'];?> >
-		<textarea name="qcontent" cols=50 rows=5></textarea><br>
+		<textarea id='content' name="qcontent" onfocus="if (this.value == 'Content') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Content';}">Content</textarea><br>
 		<?php		
 		if ($_GET['mode']==1) {
 			echo "<input type='hidden' name='qid' value='".$_POST["qid"]."'>";
@@ -20,5 +21,51 @@
 		<input type="submit" value="Post">
 		</form> 
 
+	<script>
+		function validateForm() {
+			
+			var name = document.getElementById("name").value;
+			var mail = document.getElementById("mail").value;
+			var topic = document.getElementById("topic").value;
+			var cont = document.getElementById("content").value;
+			var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+			
+	
+			if (name == null || name == "" || name == "Name") {
+				alert("Name must be filled out");
+				return false;
+			}
+			if (mail == null || mail == "" || mail == "Email") {
+				alert("Email must be filled out");
+				return false;
+			}
+			if (!validateEmail(mail)) {
+				alert("Incorrect email address");
+				return false;
+			}
+			if (topic == null || topic == "" || topic == "Question Topic") {
+				alert("Topic must be filled out");
+				return false;
+			}
+			if (cont == null || cont == "" || cont == "Content") {
+				alert("Content must be filled out");
+				return false;
+			}
+			
+			    
+		}
+		
+		function validateEmail(email) {
+			var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+			return re.test(email);
+		}
+		function validate() {
+			if (!validateForm()) {
+				returnToPreviousPage();	
+			}
+		}
+
+	
+	</script>
 	</body>
 </html>
