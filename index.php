b3226fed7cae0660b6e111ade36db9bda10a8df9
squilliams <bimoaryo@rocketmail.com>
2015-10-14 15:41:06 +0700

    - Fitur vote up dan vote down untuk question maupun answers - Menampilkan jumlah answer dari tiap question di halaman utama - Menampilkan jumlah vote dari tiap question di halaman utama - Menampilkan tanggal untuk question dan answer - Mengubah tampilan submit content untuk answer dan edit question jadi textarea

--- index.php
+++ index.php
@@ -23,8 +23,7 @@
     <h2>recently asked questions</h2>
     <?php 
     /* Membuka koneksi ke database */
-    $link = mysqli_connect("127.0.0.1", "root", "", "tugaswbd1");
-
+    include "connect.php";
     if(isset($_POST['submit']))
     {
         $name = $_POST["Name"];
@@ -32,12 +31,13 @@
         $topic = $_POST["topic"];
         $content = $_POST["content"];
         $id = $_POST["id"];
-    if ($id == "NULL"){
-        $sql="INSERT INTO qlist (name, email, topic, content)  VALUES ('$name', '$email', '$topic', '$content')";
-    }
-    else {
-        $sql="UPDATE qlist SET name='$name', email='$email', topic='$topic', content='$content' WHERE id='$id'";
+        $curdate = date("Y-m-d H:i:s");
 
+    if ($id == "NULL"){ // Buat post baru
+        $sql="INSERT INTO qlist (name, email, topic, content, datetime)  VALUES ('$name', '$email', '$topic', '$content', '$curdate')";
+    }
+    else { // edit post yang ada
+        $sql="UPDATE qlist SET name='$name', email='$email', topic='$topic', content='$content', datetime='$curdate' WHERE id='$id'";
     }
     $result=mysqli_query($link,$sql);
     }
@@ -48,9 +48,8 @@
     {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-
     // Query untuk mengambil data dari MySQL 
-    $sql="SELECT content, email, topic, id  FROM qlist ORDER BY id DESC";
+    $sql="SELECT content, email, topic, id, voteup, votedown, anscount FROM qlist ORDER BY datetime DESC";
 
     // Hasil dari query yang sudah diambil
     $result=mysqli_query($link,$sql);
@@ -62,16 +61,16 @@
     <div class="questionlist">
         <div class="angka">
             <div class="vote">
-                <div class="jlhvote">0</div>
+                <div class="jlhvote"><?php echo $row["voteup"] + $row["votedown"]; ?></div>
                 <div class="ketvote">Votes</div>
             </div>
             <div class="answers">
-                <div class="jlhvote">0</div>
+                <div class="jlhvote"><?php echo $row["anscount"]; ?></div>
                 <div class="ketvote">Answers</div>
             </div>
         </div>
         
-        <div class="question">
+        <div class="iquestion">
             <div class="questiontopic"><a href="answers.php?id=<?php echo $row["id"];?>"><?php echo $row["topic"]; ?></a></div>
             <div class="questioncontent"><p><?php echo $row["content"]; ?></p></div>
         </div>
@@ -80,7 +79,7 @@
                 <input type="hidden" name="idnya" value="<?php echo $row["id"] ?>">
                 <div class="buttonlink"><button type="submit">edit</button></div>
             </form>
-            |  <a href="#" onclick="loadXMLDoc()">delete</a></div>
+            |  <a href="#" onclick="hapusquestion()">delete</a></div>
 
     </div>
     <?php 
--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-//function loadXMLDoc() {
-//    "use strict";
-//    var xmlhttp;
-//    xmlhttp = new XMLHttpRequest();
-//    xmlhttp.onreadystatechange = function () {
-//        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-//        // do something if the page loaded successfully
-//        }
-//    };
-//    
-//    xmlhttp.open("GET", "ajax_file.php", true);
-//    xmlhttp.send();
-//}
 
-function submitform() {
-    "use strict";
-    document.forms["idnya"].submit();
+
+function votingup(qid, idnya) {
+    var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("votecountnum").innerHTML = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET", "voting.php?qid=" + qid + "&idnya=" + idnya, true);
+        xmlhttp.send();
+}
+
+function votingans(qid, idnya) {
+    var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                document.getElementById("votecountans").innerHTML = xmlhttp.responseText;
+            }
+        }
+        xmlhttp.open("GET", "voting.php?qid=" + qid + "&idnya=" + idnya, true);
+        xmlhttp.send();
 }
