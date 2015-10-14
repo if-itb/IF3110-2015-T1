9cdc2da3beaad4a54909e0be9de3a7dff9ee100e
aseham <aseham.krazzy@gmail.com>
2015-10-14 21:07:47 +0700

    very first commit
    only title and plain search bar

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+  </head>
+  <body>
+    <p align=center>Simple Stack Exchange</p>
+	<form align=center name='search' action='search.php' method=post>
+      <input type='text' name='qword'><input type='submit' value='Search'>
+    </form>
+  </body>
+</html>
