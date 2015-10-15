02f6fc11fedaca3f9ea0746d74b655de303dd916
Rahman Adianto <rahman.adianto@gmail.com>
2015-10-15 20:19:24 +0700

    Fix URL to link stylesheet, js, or img

--- application/view/layout.php
+++ application/view/layout.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Simple StackExchange</title>
-	<link rel="stylesheet" href="http://<?php echo SERVER . URL; ?>public/css/style.css">
+	<link rel="stylesheet" href="<?php echo URL; ?>public/css/style.css">
 </head>
 <body>
 	<div class="container">
@@ -11,8 +11,12 @@
 			<h1><a href="index.php">Simple StackExchange</a></h1>	
 		</div>
 
+	<?php echo URL . " " . ROOT; ?>
+
 	<?php require_once(ROOT . DS . 'library' . DS . 'routes.php'); ?>	
 		
 	</div><!-- /container -->
+
+	<script src="<?php echo URL; ?>public/js/script.js"></script>
 </body>
 </html>
--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php    
  
+ 	define('URL', '/IF3110-2015-T1/');
 	define('DS', DIRECTORY_SEPARATOR);
-	define('SERVER', $_SERVER['SERVER_NAME']);
-	define('URL', $_SERVER['REQUEST_URI']);
 	define('ROOT', dirname(dirname(__FILE__)));
 	 
 	require_once (ROOT . DS . 'library' . DS . 'class.connection.php');
