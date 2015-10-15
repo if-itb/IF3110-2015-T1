1deebbee7a892dfbb73053fdf013be112a3817c0
Aufar Gilbran <aufargilbran@gmail.com>
2015-10-15 14:09:08 +0700

    Add readme and alert when validation failed

--- public/js/form.js
+++ public/js/form.js
@@ -4,11 +4,13 @@ function validateForm() {
 	for (var i = 0; i < size; ++i) {
 		var element = elements[i];
 		if (element.value.length == 0) {
+			alert('You missed some form.');
 			return false;
 		}
 		if (element.getAttribute('name').toLowerCase() == 'email') {
 			var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 			if (re.test(element.value) === false) {
+				alert('You missed some form.');
 				return false;
 			}
 		}
