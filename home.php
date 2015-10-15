c1052d72e91b8bd9e8152f88882764c1902ed3b3
Dimpos Sitorus <dimpossitorus@gmail.com>
2015-10-15 22:31:39 +0700

    Homepage
    Create the homepage, list of question.

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<Title> Simple StackExchange </Title>
+		<link rel="stylesheet" href="styles.css" type="text/css">
+		<meta> </meta>
+	</head>
+	<body>
+		<div class="container">
+			<div class="header">
+				<h1> Simple Stack Exchange </h1>
+			</div>
+			<div>
+				<form>
+					<input type="text" name="search" placeholder="Search" size = "50" require>
+					<input type="submit" value="Search">
+				</form>
+			</div>
+			<div>
+				<p>Cannot find what you are looking for? <a href="question.php">Ask Here</a></p>
+			</div>
+			<div class='recent'>
+				<h2>Recently Asked Question</h2>
+				<? php
+					//PHP here
+					//Create the div through php
+					
+				?>
+			</div>
+		</div>
+	</body>
+</html>
