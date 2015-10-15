0fbcf9a34e6675c05021ee22d28cac5fb388f273
hanscg <hcguyz@gmail.com>
2015-10-15 13:39:16 +0700

    Icon credit

--- question.php
+++ question.php
@@ -79,8 +79,8 @@
                 <input type="submit" value="Post">
             </form>
         </div>
-        <?php
-        
-        ?>
+        <div id="credits">
+            Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a>
+        </div>
     </body>
 </html>
