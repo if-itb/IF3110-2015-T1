b1acaeb89221a9fffc90866e6dc90ae379aa83e9
gha-gozu <ghazwan.sihamudin@gmail.com>
2015-10-16 07:02:19 +0700

    Add Validation Feature
    Ask Form and Answer Form now has validation feature

--- answerForm.php
+++ answerForm.php
@@ -1,17 +1,17 @@
 <?php
 	$answerForm = '<div class="answer-form">
 		<h1>Your Answer</h1>
-		<form onsubmit="validateForm()"action="questionAnswerReq.php?qid={qid}" method="POST">
-			<div class="form">
-				<input class="formContent" type="text" name="name" placeholder="Name" size="139px">
+		<form name="answerForm" onsubmit="return validateAnsForm();" action="questionAnswerReq.php?qid={qid}" method="POST">
+			<div class="form">	
+				<input class="formContent" type="text" name="name" placeholder="Name" value="">
 			</div>
 			<div class="form">
-				<input class="formContent" type="text" name="email" placeholder="Email" size="139px">
+				<input class="formContent" type="text" name="email" placeholder="Email" value="">
 			</div>
 			<div class="textArea">
-				<textarea class="formContent" name="content" placeholder="Contents" rows="10" cols="141"></textarea>
+				<textarea class="formContent" name="content" placeholder="Contents" rows="10"></textarea>
 			</div>
-				<input class="button" type="submit" value="Post">
+				<input class="button" type="submit" value="Submit">
 		</form>
 	</div>';
 	$answerForm = str_replace("{qid}", $_GET["qid"], $answerForm);
--- js/function.js
+++ js/function.js
@@ -8,6 +8,27 @@ function validateDelete() {
     
 }
 
+function validateAnsForm() {
+	var name = document.forms["answerForm"]["name"].value;
+	var email = document.forms["answerForm"]["email"].value;
+	var content = document.forms["answerForm"]["content"].value;
+
+	if (name == null || name == "") {
+		alert("Did you forget to insert your name?");
+		return false;
+	} else if (email == null || email == "") {
+		alert("Hey, what's your email?");
+		return false;
+	} else if (content == null || content == "") {
+		alert("So what is/are your question?");
+		return false;
+	} else if ( !validateEmail(email) ) {
+		alert("Your email is invalid");
+		return false;
+	}
+	return true;
+}
+
 function validateForm(){
 	var name = document.forms["questForm"]["name"].value;
 	var email = document.forms["questForm"]["email"].value;
@@ -26,6 +47,14 @@ function validateForm(){
 	} else if (content == null || content == "") {
 		alert("So what is/are your question?");
 		return false;
+	} else if ( !validateEmail(email) ) {
+		alert("Your email is invalid");
+		return false;
 	}
 	return true;
+}
+
+function validateEmail(email) {
+	var regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return regex.test(email);
 }
