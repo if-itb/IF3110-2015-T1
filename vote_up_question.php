603f8fd5c1ac11945a23ed11b2ce080a6a6a5116
nursyahrina <nursyahrina17@gmail.com>
2015-10-16 21:28:04 +0700

    Enable add vote question

--- vote_up_question.php
+++ vote_up_question.php
@@ -1,6 +1,6 @@
 <?php
 
-			$dbname = "stackexchange";
+	    $dbname = "stackexchange";
             $host = "localhost";
             $username = "root";
             $password = "810f810m";
@@ -10,14 +10,14 @@
 
 
             $id = $_GET["id"];
-            $sql = "UPDATE question SET vote = vote + 1 WHERE questionID='$id'";
+            $sql = "UPDATE question SET vote = vote+1 WHERE questionID='$id'";
             $result = mysqli_query($conn, $sql);
     
             $sql = "SELECT vote FROM question where questionID='$id'";
             $result = mysqli_query($conn, $sql);
             $row = mysqli_fetch_assoc($result);
             if (mysqli_num_rows($result) > 0)
-                echo $row[4];
+                echo $row['vote'];
             exit;
 
 ?>
